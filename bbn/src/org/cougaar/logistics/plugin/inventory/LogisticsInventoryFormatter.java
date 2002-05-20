b29851f8b6cff8940b1d68a074b7ef706d6b4b16
mdavis <mdavis>
2002-05-20 22:03:58 +0000

    formatting excel type Tasks and ARs complete.  Tommorow xml.

--- bbn/src/org/cougaar/logistics/plugin/inventory/LogisticsInventoryFormatter.java
+++ bbn/src/org/cougaar/logistics/plugin/inventory/LogisticsInventoryFormatter.java
@@ -3,9 +3,10 @@
  *  Copyright 1997-2001 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
  * 
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the Cougaar Open Source License as published by
- *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the Cougaar Open Source License as
+ *  published by DARPA on the Cougaar Open Source Website
+ *  (www.cougaar.org).
  * 
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  * PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
@@ -86,56 +87,73 @@ public class LogisticsInventoryFormatter {
 	return taskStr;
     }
 
-    protected void logTasks(ArrayList tasks,long aCycleStamp) {
+    protected void logTasks(ArrayList tasks,
+			    long aCycleStamp,
+			    boolean expandTimestamp) {
 	cycleStamp = aCycleStamp;
 	for(int i=0; i < tasks.size(); i++) {
 	    Task aTask = (Task) tasks.get(i);
-	    String taskStr = buildTaskPrefixString(aTask);
-	    taskStr = taskStr + TimeUtils.dateString(taskUtils.getStartTime(aTask)) + ",";
-	    taskStr = taskStr + TimeUtils.dateString(taskUtils.getEndTime(aTask)) + ",";
-	    //This is qty for supply, daily rate for projection
-	    taskStr = taskStr + taskUtils.getDailyQuantity(aTask);
-	    write(taskStr);
+	    logTask(aTask,expandTimestamp);
 	}
     }
+
+    protected void logTask(Task aTask,boolean expandTimestamp) {
+	String taskStr = buildTaskPrefixString(aTask);
+	taskStr = taskStr + getDateString(taskUtils.getStartTime(aTask),expandTimestamp) + ",";
+	taskStr = taskStr + getDateString(taskUtils.getEndTime(aTask),expandTimestamp) + ",";
+	//This is qty for supply, daily rate for projection
+	taskStr = taskStr + taskUtils.getDailyQuantity(aTask);
+	writeln(taskStr);
+    } 
     
-    protected void logAllocationResults(ArrayList tasks, long aCycleStamp) {
+    protected void logAllocationResults(ArrayList tasks, long aCycleStamp, boolean expandTimestamp) {
 	cycleStamp = aCycleStamp;
 	for(int i=0; i < tasks.size(); i++) {
 	    Task aTask = (Task) tasks.get(i);
-	    PlanElement pe = aTask.getPlanElement();
-	    String resultType="REPORTED";
-	    AllocationResult ar = pe.getReportedResult();
-	    if(ar == null) {
-		resultType = "ESTIMATED";
-		ar = pe.getEstimatedResult();
-	    }
-	    if(ar != null) {
-		String taskStr = buildTaskPrefixString(aTask);
-		taskStr = taskStr + resultType + ",";
-		if(!ar.isPhased()) {
-		    String outputStr = taskStr + TimeUtils.dateString(Math.round(TaskUtils.getStartTime(ar))) + ",";
-		    outputStr = outputStr + TimeUtils.dateString(Math.round(TaskUtils.getEndTime(ar))) + ",";
-		    outputStr = outputStr + TaskUtils.getQuantity(ar) + ",";
-		    write(outputStr);
-		}
-		else {
-		    int[] ats = ar.getAspectTypes();
-		    int qtyInd = getIndexForType(ats,AspectType.QUANTITY);
-		    int startInd = getIndexForType(ats,AspectType.START_TIME);
-		    int endInd = getIndexForType(ats,AspectType.END_TIME);
-		    Enumeration phasedResults = ar.getPhasedResults();
-		    while(phasedResults.hasMoreElements()) {
-			double[] results = (double[])phasedResults.nextElement();
-			String outputStr = taskStr + TimeUtils.dateString(Math.round(results[startInd])) + ",";
-			outputStr = outputStr + TimeUtils.dateString(Math.round(results[endInd])) + ",";
-			outputStr = outputStr + results[qtyInd] + ",";
-			write(outputStr);
-		    }
-		}
-	    }
-	}    
+	    logAllocationResult(aTask, expandTimestamp);
+	}
     }
+
+    protected void logAllocationResult(Task aTask, boolean expandTimestamp) {
+	PlanElement pe = aTask.getPlanElement();
+	String resultType="REPORTED";
+	AllocationResult ar = pe.getReportedResult();
+	if(ar == null) {
+	    resultType = "ESTIMATED";
+	    ar = pe.getEstimatedResult();
+	}
+	if(ar != null) {
+	    String taskStr = buildTaskPrefixString(aTask);
+	    taskStr = taskStr + resultType + ",";
+	    if(!ar.isPhased()) {
+		String outputStr = taskStr + getDateString(TaskUtils.getStartTime(ar),expandTimestamp) + ",";
+		outputStr = outputStr + getDateString(TaskUtils.getEndTime(ar),expandTimestamp) + ",";
+		outputStr = outputStr + TaskUtils.getQuantity(ar);
+		writeln(outputStr);
+	    }
+	    else {
+		int[] ats = ar.getAspectTypes();
+		int qtyInd = getIndexForType(ats,AspectType.QUANTITY);
+		int startInd = getIndexForType(ats,AspectType.START_TIME);
+		int endInd = getIndexForType(ats,AspectType.END_TIME);
+		Enumeration phasedResults = ar.getPhasedResults();
+		while(phasedResults.hasMoreElements()) {
+		    double[] results = (double[])phasedResults.nextElement();
+		    String outputStr = taskStr;
+		    if(startInd == -1) {
+			outputStr += ",,";
+		    } 
+		    else {
+			outputStr += getDateString(results[startInd],expandTimestamp) + ",";
+		    }
+		    outputStr += getDateString(results[endInd],expandTimestamp) + ",";
+		    outputStr += results[qtyInd];
+		    writeln(outputStr);
+		}
+	    }
+	}
+    }    
+
 
     
     protected static int getIndexForType(int[] types, int type) {
@@ -147,18 +165,36 @@ public class LogisticsInventoryFormatter {
 	return -1;
     }
 
-    
+    protected static String getDateString(double datetime, boolean expandTimestamp){
+	return getDateString(Math.round(datetime),expandTimestamp);
+    }
+
+    protected static String getDateString(long datetime, boolean expandTimestamp) {
+	if(expandTimestamp) {
+	    return TimeUtils.dateString(datetime);
+	}
+	else {
+	    return Long.toString(datetime);
+	}
+    }
+
     protected void excelLogProjections(ArrayList tasks,long aCycleStamp) {
-	writeNoCycle("CYCLE,PARENT UID,UID,VERB,FOR(ORG),START TIME,END TIME,DAILY RATE");
-	logTasks(tasks,aCycleStamp);
+	writeNoCycleLn("CYCLE,PARENT UID,UID,VERB,FOR(ORG),START TIME,END TIME,DAILY RATE");
+	logTasks(tasks,aCycleStamp,true);
     } 
     
     
     protected void excelLogNonProjections(ArrayList tasks,long aCycleStamp) {
-	writeNoCycle("CYCLE,PARENT UID,UID,VERB,FOR(ORG),START TIME,END TIME,QTY");
-	logTasks(tasks,aCycleStamp);
+	writeNoCycleLn("CYCLE,PARENT UID,UID,VERB,FOR(ORG),START TIME,END TIME,QTY");
+	logTasks(tasks,aCycleStamp,true);
     } 
     
+
+    protected void excelLogARs(ArrayList tasks,long aCycleStamp) {
+	writeNoCycleLn("CYCLE,PARENT UID,TASK UID,TASK VERB,TASK FOR(ORG),AR TYPE,AR START TIME,AR END TIME,AR QTY");
+	logAllocationResults(tasks,aCycleStamp,true);
+    } 
+
     protected ArrayList buildParentTaskArrayList(ArrayList tasks) {
 	ArrayList parentList = new ArrayList(tasks.size());
 	for(int i=0; i < tasks.size() ; i++ ) {
@@ -181,34 +217,63 @@ public class LogisticsInventoryFormatter {
 	ArrayList supplyList = buildParentTaskArrayList(withdrawList);
 	ArrayList projSupplyList = buildParentTaskArrayList(projWithdrawList);
 
-	write("SUPPLY TASKS:START");
+	writeNoCycleLn("SUPPLY TASKS:START");
 	excelLogNonProjections(supplyList,aCycleStamp);
-	write("SUPPLY TASKS:END");
-	write("WITHDRAW TASKS:START");
+	writeNoCycleLn("SUPPLY TASKS:END");
+	writeNoCycleLn("WITHDRAW TASKS:START");
 	excelLogNonProjections(withdrawList,aCycleStamp);
-	write("WITHDRAW TASKS:END");
-	write("PROJECTSUPPLY TASKS:START");
+	writeNoCycleLn("WITHDRAW TASKS:END");
+	writeNoCycleLn("PROJECTSUPPLY TASKS:START");
 	excelLogProjections(projSupplyList,aCycleStamp);
-	write("PROJECTSUPPLY TASKS:END");
-	write("PROJECTWITHDRAW TASKS:START");
+	writeNoCycleLn("PROJECTSUPPLY TASKS:END");
+	writeNoCycleLn("PROJECTWITHDRAW TASKS:START");
 	excelLogNonProjections(projWithdrawList,aCycleStamp);
-	write("PROJECTWITHDRAW TASKS:END");
+	writeNoCycleLn("PROJECTWITHDRAW TASKS:END");
+
+	writeNoCycleLn("SUPPLY TASK ALLOCATION RESULTS :START");
+	excelLogARs(supplyList,aCycleStamp);
+	writeNoCycleLn("SUPPLY TASK ALLOCATION RESULTS :END");
+	writeNoCycleLn("WITHDRAW TASK ALLOCATION RESULTS :START");
+	excelLogARs(withdrawList,aCycleStamp);
+	writeNoCycleLn("WITHDRAW TASK ALLOCATION RESULTS :END");
+	writeNoCycleLn("PROJECTSUPPLY TASK ALLOCATION RESULTS :START");
+	excelLogARs(projSupplyList,aCycleStamp);
+	writeNoCycleLn("PROJECTSUPPLY TASK ALLOCATION RESULTS :END");
+	writeNoCycleLn("PROJECTWITHDRAW TASK ALLOCATION RESULTS :START");
+	excelLogARs(projWithdrawList,aCycleStamp);
+	writeNoCycleLn("PROJECTWITHDRAW TASK ALLOCATION RESULTS :END");
+
     }
 
     protected void logResupplyToExcelOutput(ArrayList resupplyList,
 					    ArrayList projResupplyList,
 					    long aCycleStamp) {
+	cycleStamp = aCycleStamp;
+
+	writeNoCycleLn("RESUPPLY SUPPLY TASKS: START");
+	excelLogNonProjections(resupplyList,aCycleStamp);
+	writeNoCycleLn("RESUPPLY SUPPLY TASKS: END");
+	writeNoCycleLn("RESUPPLY PROJECTSUPPLY TASKS: START");
+	excelLogProjections(projResupplyList,aCycleStamp);
+	writeNoCycleLn("RESUPPLY PROJECTSUPPLY TASKS: END");
+
+	writeNoCycleLn("RESUPPLY SUPPLY TASK ALLOCATION RESULTS: START");
+	excelLogARs(resupplyList,aCycleStamp);
+	writeNoCycleLn("RESUPPLY SUPPLY TASK ALLOCATION RESULTS: END");
+	writeNoCycleLn("RESUPPLY PROJECTSUPPLY TASK ALLOCATION RESULTS: START");
+	excelLogARs(projResupplyList,aCycleStamp);
+	writeNoCycleLn("RESUPPLY PROJECTSUPPLY TASK ALLOCATION RESULTS: END");
 
     }
 
-    public void write(String csvString) {
-	writeNoCycle(cycleStamp + "," + csvString);
+    public void writeln(String csvString) {
+	writeNoCycleLn(cycleStamp + "," + csvString);
     }
 
-    public void writeNoCycle(String aString) {
+    public void writeNoCycleLn(String aString) {
 	if(output != null) {
 	    try {
-		output.write(aString);
+		output.write(aString + "\n");
 	    }
 	    catch(IOException e) {		
 		logger.error("Exception trying to write to Writer: " + output, e);
