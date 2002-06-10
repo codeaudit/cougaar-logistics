05ffa2e9c04cb6fb89823115169a8ca45301130a
mdavis <mdavis>
2002-06-10 23:23:20 +0000

    Data Object root header objects - contains all the subdata objects.

---
+++ bbn/src/org/cougaar/logistics/ui/inventory/data/InventoryData.java
@@ -0,0 +1,57 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2002 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.q
+ * </copyright>
+ */
+ 
+package org.cougaar.logistics.ui.inventory.data;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import org.cougaar.logistics.plugin.inventory.LogisticsInventoryFormatter;
+
+/** 
+ * <pre>
+ * 
+ * The InventoryData holds all the Inventory Schedules
+ * data and asset name and org associated with the inventory
+ * data necessary to chart the graph.
+ *
+ **/
+
+public class InventoryData {
+    
+    protected String    item;
+    protected String    org;
+
+    protected Hashtable schedules;
+
+    public InventoryData(String itemName,String anOrg) {
+	item = itemName;
+	org = anOrg;
+	schedules = new Hashtable(20);
+    }
+
+    public void addSchedule(InventoryScheduleHeader schedule) {
+	schedules.put(schedule.getName(),schedule);
+    }
+
+    public Hashtable getSchedules() { return schedules; }
+}
+
+
---
+++ bbn/src/org/cougaar/logistics/ui/inventory/data/InventoryScheduleHeader.java
@@ -0,0 +1,79 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2002 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.q
+ * </copyright>
+ */
+ 
+package org.cougaar.logistics.ui.inventory.data;
+
+import java.util.ArrayList;
+import org.cougaar.logistics.plugin.inventory.LogisticsInventoryFormatter;
+
+
+/** 
+ * <pre>
+ * 
+ * The InventoryScheduleHeader holds the schedule of all the
+ * elements in a given schedule
+ *
+ **/
+
+
+
+public class InventoryScheduleHeader{
+    
+    public final static int TASKS_TYPE=1;
+    public final static int PROJ_TASKS_TYPE=2;
+    public final static int ARS_TYPE=3;
+    public final static int PROJ_ARS_TYPE=4;
+    public final static int LEVELS_TYPE=5;
+    
+    protected String    name;
+    protected ArrayList schedule;
+    protected int       type;
+
+    public InventoryScheduleHeader(String aName,int aType, ArrayList aSchedule) {
+	name = aName;
+	schedule = aSchedule;
+	type = aType;
+    }
+
+    public InventoryScheduleHeader(String aName,String aType, ArrayList aSchedule) {
+	this(aName,getTypeInt(aType),aSchedule);
+    }
+
+    public String getName() { return name; }
+    public ArrayList getSchedule() {return schedule; }
+    public int getType() { return type; }
+
+    public static int getTypeInt(String aType) {
+	if(aType.equals(LogisticsInventoryFormatter.TASKS_TYPE))
+	    return InventoryScheduleHeader.TASKS_TYPE;
+	if(aType.equals(LogisticsInventoryFormatter.PROJ_TASKS_TYPE))
+	    return InventoryScheduleHeader.PROJ_TASKS_TYPE;
+	if(aType.equals(LogisticsInventoryFormatter.ARS_TYPE))
+	    return InventoryScheduleHeader.ARS_TYPE;
+	if(aType.equals(LogisticsInventoryFormatter.PROJ_ARS_TYPE))
+	    return InventoryScheduleHeader.PROJ_ARS_TYPE;
+	if(aType.equals(LogisticsInventoryFormatter.LEVELS_TYPE))
+	    return InventoryScheduleHeader.LEVELS_TYPE;
+	return -1;
+    }
+}
+
+
