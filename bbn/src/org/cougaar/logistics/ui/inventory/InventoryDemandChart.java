0823668cdd6bf3a9b071ef32621de5d6edf02924
mdavis <mdavis>
2002-07-03 16:05:33 +0000

    This is the chart that reflects the demand portion of the inventory: includes the withdraws, withdraw allocation results,  counted projections, and counted projection results.

---
+++ bbn/src/org/cougaar/logistics/ui/inventory/InventoryDemandChart.java
@@ -0,0 +1,101 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2002 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.q
+ * </copyright>
+ */
+ 
+package org.cougaar.logistics.ui.inventory;
+
+import java.util.Date;
+
+
+import java.awt.Color;
+import java.awt.Insets;
+
+
+import com.klg.jclass.chart.JCChart;
+import com.klg.jclass.chart.JCPickListener;
+import com.klg.jclass.chart.data.JCDefaultDataSource;
+import com.klg.jclass.util.swing.JCExitFrame;
+import com.klg.jclass.chart.JCSymbolStyle;
+
+import com.klg.jclass.chart.ChartDataView;
+import com.klg.jclass.chart.ChartDataViewSeries;
+import com.klg.jclass.chart.ChartDataModel;
+import com.klg.jclass.chart.ChartDataEvent;
+
+import org.cougaar.logistics.plugin.inventory.LogisticsInventoryFormatter;
+import org.cougaar.logistics.plugin.inventory.TimeUtils;
+
+import com.klg.jclass.util.legend.JCLegend;
+import com.klg.jclass.util.legend.JCMultiColLegend;
+
+import org.cougaar.logistics.ui.inventory.data.InventoryData;
+
+/** 
+ * <pre>
+ * 
+ * The InventoryDemandChart class is the chart class for
+ * displaying demand information.   It plots in 
+ * 2 views, 2 series apiece: demand requisitions, and
+ * their corresponding allocation results and demand
+ * projection and their allocation results.
+ * 
+ * @see InventoryChart
+ * @see RequisitionChartDataModel
+ * @see ProjectionChartDataModel
+ * 
+ *
+ **/
+
+public class InventoryDemandChart extends InventoryChart {
+
+    public final static String DEMAND_TASKS =
+	LogisticsInventoryFormatter.WITHDRAW_TASKS_TAG;
+    public final static String DEMAND_ARS =
+	LogisticsInventoryFormatter.WITHDRAW_TASK_ARS_TAG;
+    public final static String DEMAND_PROJ_TASKS =
+	LogisticsInventoryFormatter.COUNTED_PROJECTWITHDRAW_TASKS_TAG;
+    public final static String DEMAND_PROJ_ARS =
+    	LogisticsInventoryFormatter.COUNTED_PROJECTWITHDRAW_TASK_ARS_TAG;
+
+
+    public InventoryDemandChart() {
+	initialize("Demand");
+    }
+
+    public void initializeChart() {
+	RequisitionsChartDataModel reqDM = 
+	    new RequisitionsChartDataModel(DEMAND_TASKS,
+					   DEMAND_ARS);
+
+	ProjectionsChartDataModel projDM = 
+	    new ProjectionsChartDataModel(DEMAND_PROJ_TASKS,
+					  DEMAND_PROJ_ARS,
+					  DEMAND_TASKS,
+					  reqDM,
+					  false);
+
+	ChartDataView projChartDataView = addView(JCChart.BAR, projDM);
+	ChartDataView reqChartDataView = addView(JCChart.BAR, reqDM);
+	
+	setBarChartColors();
+		    
+    }
+ 
+}
