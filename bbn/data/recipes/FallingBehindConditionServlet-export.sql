13bc06f973df75e3fc225a9724a377126fbf20be
bdepass <bdepass>
2002-07-12 18:05:38 +0000

    adding in recipes for the ConditionServlet which acts as a standin for the sensors and the recipe for the stimulator servlet.

---
+++ bbn/data/recipes/FallingBehindConditionServlet-export.sql
@@ -0,0 +1,30 @@
+# MySQL dump 8.16
+#
+# Host: localhost    Database: tempcopy
+#--------------------------------------------------------
+# Server version	3.23.44-nt
+
+#
+# Dumping data for table 'v4_lib_mod_recipe'
+#
+
+LOCK TABLES v4_lib_mod_recipe WRITE;
+REPLACE INTO v4_lib_mod_recipe (MOD_RECIPE_LIB_ID, NAME, JAVA_CLASS, DESCRIPTION) VALUES ('RECIPE-0005FallingBehindConditionServlet','FallingBehindConditionServlet','org.cougaar.tools.csmart.recipe.SpecificInsertionRecipe','No description available');
+UNLOCK TABLES;
+
+#
+# Dumping data for table 'v4_lib_mod_recipe_arg'
+#
+
+LOCK TABLES v4_lib_mod_recipe_arg WRITE;
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Class Name',2.000000000000000000000000000000,'org.cougaar.logistics.servlet.ConditionComponent');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Component Name',6.000000000000000000000000000000,'FallingBehindConditionServlet');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Component Priority',8.000000000000000000000000000000,'COMPONENT');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Number of Arguments',0.000000000000000000000000000000,'3');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Target Component Selection Query',4.000000000000000000000000000000,'recipeQueryAllAgents');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Type of Insertion',7.000000000000000000000000000000,'plugin');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 1',5.000000000000000000000000000000,'org.cougaar.logistics.servlet.ConditionServlet');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 2',3.000000000000000000000000000000,'/CPUSettingServlet');
+REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 3',1.000000000000000000000000000000,'conditionName=FallingBehind');
+UNLOCK TABLES;
+
