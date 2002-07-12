965ea5c9a9429c94fb364583e64f672eb1e63847
bdepass <bdepass>
2002-07-12 21:42:04 +0000

    change these to work with the 9.4.1 head (remove v4 and v6 references)

--- bbn/data/recipes/FallingBehindConditionServlet-export.sql
+++ bbn/data/recipes/FallingBehindConditionServlet-export.sql
@@ -5,26 +5,26 @@
 # Server version	3.23.44-nt
 
 #
-# Dumping data for table 'v4_lib_mod_recipe'
+# Dumping data for table 'lib_mod_recipe'
 #
 
-LOCK TABLES v4_lib_mod_recipe WRITE;
-REPLACE INTO v4_lib_mod_recipe (MOD_RECIPE_LIB_ID, NAME, JAVA_CLASS, DESCRIPTION) VALUES ('RECIPE-0005FallingBehindConditionServlet','FallingBehindConditionServlet','org.cougaar.tools.csmart.recipe.SpecificInsertionRecipe','No description available');
+LOCK TABLES lib_mod_recipe WRITE;
+REPLACE INTO lib_mod_recipe (MOD_RECIPE_LIB_ID, NAME, JAVA_CLASS, DESCRIPTION) VALUES ('RECIPE-0005FallingBehindConditionServlet','FallingBehindConditionServlet','org.cougaar.tools.csmart.recipe.SpecificInsertionRecipe','No description available');
 UNLOCK TABLES;
 
 #
-# Dumping data for table 'v4_lib_mod_recipe_arg'
+# Dumping data for table 'lib_mod_recipe_arg'
 #
 
-LOCK TABLES v4_lib_mod_recipe_arg WRITE;
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Class Name',2.000000000000000000000000000000,'org.cougaar.logistics.servlet.ConditionComponent');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Component Name',6.000000000000000000000000000000,'FallingBehindConditionServlet');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Component Priority',8.000000000000000000000000000000,'COMPONENT');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Number of Arguments',0.000000000000000000000000000000,'3');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Target Component Selection Query',4.000000000000000000000000000000,'recipeQueryAllAgents');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Type of Insertion',7.000000000000000000000000000000,'plugin');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 1',5.000000000000000000000000000000,'org.cougaar.logistics.servlet.ConditionServlet');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 2',3.000000000000000000000000000000,'/CPUSettingServlet');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 3',1.000000000000000000000000000000,'conditionName=FallingBehind');
+LOCK TABLES lib_mod_recipe_arg WRITE;
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Class Name',2.000000000000000000000000000000,'org.cougaar.logistics.servlet.ConditionComponent');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Component Name',6.000000000000000000000000000000,'FallingBehindConditionServlet');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Component Priority',8.000000000000000000000000000000,'COMPONENT');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Number of Arguments',0.000000000000000000000000000000,'3');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Target Component Selection Query',4.000000000000000000000000000000,'recipeQueryAllAgents');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Type of Insertion',7.000000000000000000000000000000,'plugin');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 1',5.000000000000000000000000000000,'org.cougaar.logistics.servlet.ConditionServlet');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 2',3.000000000000000000000000000000,'/CPUSettingServlet');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-0005FallingBehindConditionServlet','Value 3',1.000000000000000000000000000000,'conditionName=FallingBehind');
 UNLOCK TABLES;
 
--- bbn/data/recipes/StimulatorServletAddition-export.sql
+++ bbn/data/recipes/StimulatorServletAddition-export.sql
@@ -5,25 +5,25 @@
 # Server version	3.23.43-nt
 
 #
-# Dumping data for table 'v4_lib_mod_recipe'
+# Dumping data for table 'lib_mod_recipe'
 #
 
-LOCK TABLES v4_lib_mod_recipe WRITE;
-REPLACE INTO v4_lib_mod_recipe (MOD_RECIPE_LIB_ID, NAME, JAVA_CLASS, DESCRIPTION) VALUES ('RECIPE-StimServlet','StimulatorServletAddition','org.cougaar.tools.csmart.recipe.SpecificInsertionRecipe','No description available');
+LOCK TABLES lib_mod_recipe WRITE;
+REPLACE INTO lib_mod_recipe (MOD_RECIPE_LIB_ID, NAME, JAVA_CLASS, DESCRIPTION) VALUES ('RECIPE-StimServlet','StimulatorServletAddition','org.cougaar.tools.csmart.recipe.SpecificInsertionRecipe','No description available');
 UNLOCK TABLES;
 
 #
-# Dumping data for table 'v4_lib_mod_recipe_arg'
+# Dumping data for table 'lib_mod_recipe_arg'
 #
 
-LOCK TABLES v4_lib_mod_recipe_arg WRITE;
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Class Name',4.000000000000000000000000000000,'org.cougaar.core.servlet.BlackboardServletComponent');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Component Name',0.000000000000000000000000000000,'org.cougaar.core.servlet.BlackboardServletComponent');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Component Priority',2.000000000000000000000000000000,'COMPONENT');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Number of Arguments',3.000000000000000000000000000000,'2');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Target Component Selection Query',5.000000000000000000000000000000,'recipeQueryAllAgents');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Type of Insertion',1.000000000000000000000000000000,'plugin');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Value 1',6.000000000000000000000000000000,'org.cougaar.glm.servlet.GLMStimulatorServlet');
-REPLACE INTO v4_lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Value 2',7.000000000000000000000000000000,'/stimulator');
+LOCK TABLES lib_mod_recipe_arg WRITE;
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Class Name',4.000000000000000000000000000000,'org.cougaar.core.servlet.BlackboardServletComponent');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Component Name',0.000000000000000000000000000000,'org.cougaar.core.servlet.BlackboardServletComponent');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Component Priority',2.000000000000000000000000000000,'COMPONENT');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Number of Arguments',3.000000000000000000000000000000,'2');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Target Component Selection Query',5.000000000000000000000000000000,'recipeQueryAllAgents');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Type of Insertion',1.000000000000000000000000000000,'plugin');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Value 1',6.000000000000000000000000000000,'org.cougaar.glm.servlet.GLMStimulatorServlet');
+REPLACE INTO lib_mod_recipe_arg (MOD_RECIPE_LIB_ID, ARG_NAME, ARG_ORDER, ARG_VALUE) VALUES ('RECIPE-StimServlet','Value 2',7.000000000000000000000000000000,'/stimulator');
 UNLOCK TABLES;
 
