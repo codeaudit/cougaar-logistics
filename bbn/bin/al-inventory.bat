2d77e5698d846b90af287f2c9c10510facf196ad
mdavis <mdavis>
2002-07-17 14:36:51 +0000

    Took out reference to Log4jInit.txt file.

--- bbn/bin/al-inventory.bat
+++ bbn/bin/al-inventory.bat
@@ -25,8 +25,8 @@ set MYCLASSES=org.cougaar.logistics.ui.inventory.InventoryUIFrame
 REM set MYCLASSES=org.cougaar.logistics.ui.inventory.data.InventoryProjAR
 set BS=org.cougaar.bootstrap.Bootstrapper
 set MYMEMORY=-Xms100m -Xmx300m
-set MYPROPERTIES= -Dorg.cougaar.config.path=%COUGAAR_INSTALL_PATH%\configs\common -Dorg.cougaar.core.logging.config.filename=Log4jInit.txt -D
+REM set MYPROPERTIES= -Dorg.cougaar.config.path=%COUGAAR_INSTALL_PATH%\configs\common -Dorg.cougaar.core.logging.config.filename=Log4jInit.txt
 
 @ECHO ON
 
-java.exe %MYMEMORY% %MYPROPERTIES% -classpath %MYCLASSPATH% %BS% %MYCLASSES% %1
+java.exe %MYMEMORY% -classpath %MYCLASSPATH% %BS% %MYCLASSES% %1
