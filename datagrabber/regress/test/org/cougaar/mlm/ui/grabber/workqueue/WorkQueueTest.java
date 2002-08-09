29754a4b2d410727e5f4c99acc18ea637b0c7ddd
tom <tom>
2002-08-09 13:38:08 +0000

    more tests

--- datagrabber/regress/test/org/cougaar/mlm/ui/grabber/workqueue/WorkQueueTest.java
+++ datagrabber/regress/test/org/cougaar/mlm/ui/grabber/workqueue/WorkQueueTest.java
@@ -7,6 +7,10 @@ package test.org.cougaar.mlm.ui.grabber.workqueue;
 
 import junit.framework.TestCase;
 import org.cougaar.mlm.ui.grabber.workqueue.WorkQueue;
+import org.cougaar.mlm.ui.grabber.workqueue.Work;
+import org.cougaar.mlm.ui.grabber.workqueue.Result;
+import org.cougaar.mlm.ui.grabber.logger.Logger;
+import org.cougaar.mlm.ui.grabber.logger.StdLogger;
 
 public class WorkQueueTest extends TestCase {
 
@@ -15,7 +19,7 @@ public class WorkQueueTest extends TestCase {
     }
 
     public void testBasic() {
-        WorkQueue wq = new WorkQueue(null, null);
+        WorkQueue wq = new WorkQueue(new StdLogger(), null);
         assertTrue(wq.getWorkIDToStatusMap().isEmpty());
         assertEquals(0, wq.getNumActiveWork());
         assertTrue(!wq.isBusy());
@@ -25,9 +29,34 @@ public class WorkQueueTest extends TestCase {
     }
 
     public void testID() {
-        WorkQueue wq = new WorkQueue(null, null);
+        WorkQueue wq = new WorkQueue(new StdLogger(), null);
         int first = wq.getValidID();
         assertTrue(wq.getValidID() > first);
+    }
+
+    public void testWork() {
+        WorkQueue wq = new WorkQueue(new StdLogger(), null);
+        final int id = wq.getValidID();
+        wq.enque(new Work() {
+            public int getID() {
+                return id;
+            }
+
+            public String getStatus() {
+                return "test";
+            }
+
+            public void halt() {
+            }
+
+            public Result perform(Logger l) {
+                try {
+                    Thread.currentThread().sleep(2000);
+                } catch (Exception e) {}
+                return null;
+            }
+        });
+        assertTrue(wq.isBusy());
     }
 
 
