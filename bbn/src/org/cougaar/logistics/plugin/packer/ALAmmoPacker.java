368027f9ca9f3e6aea444d475b264ff6462f3660
gvidaver <gvidaver>
2002-08-17 00:16:45 +0000

    fix for problem where would not fill in quantity aspect in estimated allocation result

--- bbn/src/org/cougaar/logistics/plugin/packer/ALAmmoPacker.java
+++ bbn/src/org/cougaar/logistics/plugin/packer/ALAmmoPacker.java
@@ -20,6 +20,15 @@ import org.cougaar.glm.ldm.asset.Ammunition;
 import org.cougaar.glm.packer.Packer;
 import org.cougaar.glm.packer.AggregationClosure;
 import org.cougaar.glm.packer.AmmoTransport;
+import org.cougaar.core.plugin.util.PluginHelper;
+import org.cougaar.planning.ldm.plan.AllocationResult;
+import org.cougaar.planning.ldm.plan.AllocationResultDistributor;
+import org.cougaar.planning.ldm.plan.AspectType;
+import org.cougaar.planning.ldm.plan.AspectValue;
+import org.cougaar.planning.ldm.plan.PlanElement;
+import org.cougaar.planning.ldm.plan.Task;
+import org.cougaar.planning.ldm.plan.Verb;
+import org.cougaar.core.blackboard.IncrementalSubscription;
 
 /**
  * ALAmmoPacker - handles packing ammo supply requests
@@ -72,13 +81,67 @@ public class ALAmmoPacker extends Packer {
   protected Collection groupByAggregationClosure(Collection tasks) {
     return AmmoTransport.getTransportGroups(tasks);
   }
+  protected void updateAllocationResult(IncrementalSubscription planElements) {
+    // Make sure that quantity preferences get returned on the allocation
+    // results. Transport thread may not have filled them in.
+    Enumeration changedPEs = planElements.getChangedList();
+    while (changedPEs.hasMoreElements()) {
+      PlanElement pe = (PlanElement)changedPEs.nextElement();
+      
+      if (PluginHelper.updatePlanElement(pe)) {
+        boolean needToCorrectQuantity = false;
+        
+        AllocationResult estimatedAR = pe.getEstimatedResult();
+        double prefValue = 
+          pe.getTask().getPreference(AspectType.QUANTITY).getScoringFunction().getBest().getAspectValue().getValue();
+        
+        AspectValue[] aspectValues = estimatedAR.getAspectValueResults();
+        
+	boolean foundQuantity = false;
+        for (int i = 0; i < aspectValues.length; i++) {
+          if (aspectValues[i].getAspectType() == AspectType.QUANTITY) {
+            if (aspectValues[i].getValue() != prefValue) {
+              // set the quantity to be the preference quantity
+              aspectValues[i].setValue(prefValue); 
+              needToCorrectQuantity = true;
+            }
+	    foundQuantity = true;
+            break;
+          }
+        }
+
+	// BOZO - 
+	// Gordon Vidaver 08/16/02 - 
+	// someone should really fix this eventually
+	// some expansion is not setting the QUANTITY aspect on the aspect value, and Inventory Manager
+	// gets upset about this missing aspect in findCommittedRefill
+	if (!foundQuantity) {
+	  AspectValue [] copy = new AspectValue [aspectValues.length+1];
+	  System.arraycopy(aspectValues,0,copy,0,aspectValues.length);
+	  copy[aspectValues.length] = new AspectValue (AspectType.QUANTITY, prefValue);
+	  aspectValues=copy;
+	  if (getLoggingService().isDebugEnabled()) {
+            getLoggingService().debug("Packer.updateAllocationResult - fixing quantity on estimated AR of pe " + pe.getUID() + 
+				      " task " + pe.getTask().getUID());
+	  }
+	}
+
+        if (needToCorrectQuantity || !foundQuantity) {
+	  if (getLoggingService().isDebugEnabled()) {
+            getLoggingService().debug("Packer.updateAllocationResult - fixing quantity on estimated AR of pe " + pe.getUID());
+	  } 
+
+          AllocationResult correctedAR = 
+            new AllocationResult(estimatedAR.getConfidenceRating(),
+                                 estimatedAR.isSuccess(),
+                                 aspectValues);
+
+          pe.setEstimatedResult(correctedAR);          
+        }
+        
+        
+        publishChange(pe);
+      }
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
