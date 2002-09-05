b8a7b42d8d18fad4f9ee64f4aad66f791f3d8dc9
gvidaver <gvidaver>
2002-09-05 19:09:28 +0000

    fix for bug #2118

--- datagrabber/src/org/cougaar/mlm/ui/newtpfdd/gui/view/details/AssetAssetDetailRequest.java
+++ datagrabber/src/org/cougaar/mlm/ui/newtpfdd/gui/view/details/AssetAssetDetailRequest.java
@@ -34,7 +34,7 @@ import java.sql.SQLException;
 
 /**
  * Gets data about assets on a given asset
- * @author Benjamin Lubin; last modified by: $Author: tom $
+ * @author Benjamin Lubin; last modified by: $Author: gvidaver $
  *
  * @since 4/27/01
  **/
@@ -57,6 +57,9 @@ public class AssetAssetDetailRequest extends AssetDetailRequest{
   //////////
 
   protected String getSql(DatabaseConfig dbConfig, int runID){
+    if (assetID.indexOf ("-item-") != -1)
+      return getManifestSql(dbConfig, runID);
+
     //For select:
     String aiOwner="ai."+DGPSPConstants.COL_OWNER;
     String apType="ap."+DGPSPConstants.COL_ALP_TYPEID;
@@ -99,6 +102,49 @@ public class AssetAssetDetailRequest extends AssetDetailRequest{
       return sql;
   }
 
+  protected String getManifestSql(DatabaseConfig dbConfig, int runID){
+    //For select:
+    String aiOwner="ai."+DGPSPConstants.COL_OWNER;
+    String apType="ap."+DGPSPConstants.COL_ALP_TYPEID;
+    String apNomen="ap."+DGPSPConstants.COL_ALP_NOMENCLATURE;
+    String aiName="ai."+DGPSPConstants.COL_NAME;
+    String aiAgg="ai."+DGPSPConstants.COL_AGGREGATE;
+    String apAClass="ap."+DGPSPConstants.COL_ASSET_CLASS;
+    String apAType="ap."+DGPSPConstants.COL_ASSET_TYPE;
+    String cccDimWeight="man." + DGPSPConstants.COL_WEIGHT;
+    String cccDimWidth=DGPSPConstants.COL_WIDTH;
+    String cccDimHeight=DGPSPConstants.COL_HEIGHT;
+    String cccDimDepth=DGPSPConstants.COL_DEPTH;
+    String cccDimArea=DGPSPConstants.COL_AREA;
+    String cccDimVolume=DGPSPConstants.COL_VOLUME;
+
+    String cccDimCCC=DGPSPConstants.COL_CARGO_CAT_CODE;
+
+    String sql="select "+
+      aiOwner+", "+
+      apType+", "+
+      apNomen+", "+
+      aiName+", "+
+      aiAgg+", "+
+      apAClass+", "+
+      apAType+", "+
+      cccDimWeight+", "+
+      cccDimWidth+", "+
+      cccDimHeight+", "+
+      cccDimDepth+", "+
+      cccDimArea+", "+
+      cccDimVolume+", "+
+      cccDimCCC+
+      "\nfrom " +getManifestFrom  (runID) + "\n" +
+      "where "+getManifestWhere (runID) + "\n" +
+      "order by " + aiOwner +"," +apType+","+apNomen+","+aiName;
+
+    if (debug) 
+      TPFDDLoggerFactory.createLogger().logMessage(Logger.NORMAL, Logger.GENERIC, "AssetAssetDetailRequest.getSql - sql is\n" + sql);
+
+      return sql;
+  }
+
   protected String getFrom (int runID) {
     String aiTable  =getTableName(DGPSPConstants.ASSET_INSTANCE_TABLE,runID);
     String apTable  =getTableName(DGPSPConstants.ASSET_PROTOTYPE_TABLE,runID);
@@ -106,6 +152,16 @@ public class AssetAssetDetailRequest extends AssetDetailRequest{
 
     return aiTable+" ai, "+   
       apTable+" ap, " + cccDimTable;
+  }
+
+  protected String getManifestFrom (int runID) {
+    String aiTable  =getTableName(DGPSPConstants.ASSET_INSTANCE_TABLE,runID);
+    String apTable  =getTableName(DGPSPConstants.ASSET_PROTOTYPE_TABLE,runID);
+    String cccDimTable=getTableName(DGPSPConstants.CARGO_CAT_CODE_DIM_TABLE,runID);
+    String manifestTable = getTableName(DGPSPConstants.MANIFEST_TABLE,runID);
+
+    return aiTable+" ai, "+   
+      apTable+" ap, " + cccDimTable + ", " + manifestTable + " man ";
   }
 
   protected String getWhere (int runID) {
@@ -116,6 +172,25 @@ public class AssetAssetDetailRequest extends AssetDetailRequest{
     String cccDimProtoID=cccDimTable +"."+DGPSPConstants.COL_PROTOTYPEID;
 
     return aiAssetID+"='"+assetID+"'\n and "+
+      aiProtoID     +"="+apProtoID + "\n and " +
+      cccDimProtoID +"="+apProtoID;
+  }
+
+  protected String getManifestWhere (int runID) {
+    String aiAssetID="ai."+DGPSPConstants.COL_ASSETID;
+    String aiProtoID="ai."+DGPSPConstants.COL_PROTOTYPEID;
+    String apProtoID="ap."+DGPSPConstants.COL_PROTOTYPEID;
+    String cccDimTable=getTableName(DGPSPConstants.CARGO_CAT_CODE_DIM_TABLE,runID);
+    String cccDimProtoID=cccDimTable +"."+DGPSPConstants.COL_PROTOTYPEID;
+    String manifestTable = getTableName(DGPSPConstants.MANIFEST_TABLE,runID);
+
+    String manifestInstanceID = "man." + DGPSPConstants.COL_MANIFEST_ITEM_ID;
+    String manifestName      = "man." + DGPSPConstants.COL_NAME;
+    String manifestAssetID   = "man." + DGPSPConstants.COL_ASSETID;
+
+    return 
+      manifestInstanceID +"='"+assetID + "'\n and " +
+      manifestAssetID +"="+aiAssetID + "\n and " +
       aiProtoID     +"="+apProtoID + "\n and " +
       cccDimProtoID +"="+apProtoID;
   }
