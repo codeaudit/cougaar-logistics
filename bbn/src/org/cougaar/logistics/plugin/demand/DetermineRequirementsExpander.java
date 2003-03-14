c30d05faf477ef25f1d677abae71dc97d836b278
mdavis <mdavis>
2003-03-14 16:15:36 +0000

    Fleshed out the DetermineRequirementsExpander to the point where it should expand the first DetermineRequirementsTask successfully.   Additions  or removals of MEI assets from the agent are not handled yet.  Not handling result aggregation yet either.

--- bbn/src/org/cougaar/logistics/plugin/demand/DetermineRequirementsExpander.java
+++ bbn/src/org/cougaar/logistics/plugin/demand/DetermineRequirementsExpander.java
@@ -23,7 +23,15 @@ package org.cougaar.logistics.plugin.demand;
 
 
 import java.util.Collection;
-import org.cougaar.planning.ldm.plan.Task;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.Iterator;
+import java.util.Enumeration;
+import org.cougaar.planning.ldm.asset.Asset;
+import org.cougaar.planning.ldm.plan.*;
+
+import org.cougaar.glm.ldm.Constants;
+
 
 
 /** 
@@ -46,10 +54,163 @@ public class DetermineRequirementsExpander extends DemandForecastModule implemen
     /** 
      * Expand DetermineRequirements tasks into GenerateProjections tasks. 
      **/
-    public void expandDetermineRequirements(Task detReqTask, Collection assets) {
+    public void expandDetermineRequirements(Task detReqTask, Collection assets)    {
+	if((assets == null) || (assets.isEmpty())) {
+	    disposeOfTask(detReqTask);
+	    return;
+	}
+	ArrayList gpTasks = new ArrayList();
+	Iterator assetIT = assets.iterator();
+	while(assetIT.hasNext()) {
+	    Asset consumer = (Asset) assetIT.next();
+	    NewTask gpTask = createGPTask(detReqTask, consumer);
+	    gpTasks.add(gpTask);
+	}
+	if(gpTasks.isEmpty()) {
+	    logger.warn("Cannot expand - no subtasks for determine requirements task " 
+			+ getTaskUtils().taskDesc(detReqTask));
+	}
+	else {
+	    PlanElement pe = detReqTask.getPlanElement();
+	    if((pe!=null) && (pe instanceof Disposition)) {
+		dfPlugin.publishRemove(pe);
+		pe = null;
+	    }
+	    // First time through build a fresh expansion
+	    if(pe == null) {
+		createAndPublishExpansion(detReqTask, gpTasks);
+	    }
+	    // There are new assets to add to the expansion
+	    else if(pe instanceof Expansion){
+
+	    }
+	    else {
+		logger.error("Unhandled plan element type on DetermineRequirementsTask :" + pe.getClass().getName());
+	    }
+	}	    
 	
     }
 
+    public void removeSubtasksFromDetermineRequirements(Task detReqTask, Collection removedAssets) {
+
+
+    }
+
+    protected void createAndPublishExpansion(Task parent, Collection subtasks) {
+	Iterator subtasksIT = subtasks.iterator();
+	while(subtasksIT.hasNext()) {
+	    dfPlugin.publishAdd((Task) subtasksIT.next());
+	}
+	Workflow wf = buildWorkflow(parent, subtasks);
+	Expansion expansion = getPlanningFactory().createExpansion(parent.getPlan(), parent, wf, null);
+	dfPlugin.publishAdd(expansion);
+    }
+
+    protected void addNewTasksToExpansion(Task parentTask, Collection subtasks) {
+
+    }
+
+    protected NewTask createGPTask(Task parentTask, Asset consumer) {
+	Vector prefs = new Vector();
+
+	PrepositionalPhrase prepPhrase = newPrepositionalPhrase (Constants.Preposition.OFTYPE, dfPlugin.getSupplyType());
+
+	prefs.addElement(createStartTimePref(parentTask));
+
+	NewTask newTask = getPlanningFactory().newTask();  
+	
+        newTask.setParentTask(parentTask);
+	newTask.setPlan(parentTask.getPlan());
+	newTask.setPrepositionalPhrases(parentTask.getPrepositionalPhrases());
+	
+	newTask.setDirectObject(consumer);
+	newTask.setVerb(Verb.getVerb(Constants.Verb.GENERATEPROJECTIONS));
+
+	newTask = addPrepositionalPhrase(newTask, prepPhrase);
+	newTask.setPreferences(prefs.elements());
+
+	return newTask;	
+    }
+
+    /**
+     *  Build a workflow from a vector of tasks.
+     * @param parent parent task of workflow
+     * @param subtasks workflow tasks
+     * @return Workflow 
+     **/
+    public Workflow buildWorkflow(Task parent, Collection subtasks) {
+	NewWorkflow wf = getPlanningFactory().newWorkflow();
+	wf.setParentTask(parent);
+	wf.setIsPropagatingToSubtasks(true);
+	NewTask t;
+	Iterator subtasksIT = subtasks.iterator();
+	while(subtasksIT.hasNext()) {
+	    t = (NewTask) subtasksIT.next();
+	    t.setWorkflow(wf);
+	    wf.addTask(t);
+	}
+	return wf;
+    }
+
+
+    protected void disposeOfTask(Task task) {
+	double rating = 0;
+	boolean success = true;
+	int [] aspects = {15};
+	double [] results = {0.0};
+	AllocationResult dispAR =
+	    getPlanningFactory().newAllocationResult (rating, success, aspects, results);
+	Disposition disposition =
+	    getPlanningFactory().createDisposition (task.getPlan(), task, dispAR);
+	dfPlugin.publishAdd(disposition);				
+    }
+    
+    protected long getStartTimePref (Task task) {
+	synchronized (task) {
+	    Enumeration taskPrefs = task.getPreferences();
+	    while (taskPrefs.hasMoreElements()) {
+		Preference aPref = (Preference) taskPrefs.nextElement();
+		if (aPref.getAspectType() == AspectType.START_TIME) {
+		    ScoringFunction sf = aPref.getScoringFunction();
+		    return (long) sf.getBest().getValue();
+		}
+	    }
+	}
+	return 0L;
+    }
+    
+    protected Preference createStartTimePref(Task parentTask) {
+	long startTime = getStartTimePref (parentTask);
+	AspectValue av = AspectValue.newAspectValue(AspectType.START_TIME, startTime);
+ 	ScoringFunction score = ScoringFunction.createNearOrAbove(av, 0);
+	return getPlanningFactory().newPreference(AspectType.START_TIME, score);
+    }
+
+    public PrepositionalPhrase newPrepositionalPhrase(String preposition,
+						      Object io) {
+	NewPrepositionalPhrase pp = getPlanningFactory().newPrepositionalPhrase();
+	pp.setPreposition(preposition); 
+       	pp.setIndirectObject(io);
+	return pp;
+    }
+
+
+    public static NewTask addPrepositionalPhrase(NewTask task, PrepositionalPhrase pp)
+    {
+	Enumeration enum = task.getPrepositionalPhrases();
+	if (!enum.hasMoreElements())
+	    task.setPrepositionalPhrase(pp);
+	else {
+	    Vector phrases = new Vector();
+	    while (enum.hasMoreElements()) {
+		phrases.addElement(enum.nextElement());
+	    }
+	    phrases.addElement(pp);
+	    task.setPrepositionalPhrases(phrases.elements());
+	}
+	return task;
+  } 
+    
 }
     
   
