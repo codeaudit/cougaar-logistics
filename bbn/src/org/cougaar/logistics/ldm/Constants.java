ecef7ce126e486d3147e90434264558d4cc4cc99
mdavis <mdavis>
2003-05-27 22:49:00 +0000

    Making albbn have it's own domain, so that it can start to manage its own constants.   Trying to break albbn away from glm.

---
+++ bbn/src/org/cougaar/logistics/ldm/Constants.java
@@ -0,0 +1,60 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2003 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.logistics.ldm;
+
+
+import org.cougaar.planning.ldm.plan.AspectType;
+
+public class Constants extends org.cougaar.glm.ldm.Constants {
+  protected Constants() {super();}
+
+  public interface Verb extends org.cougaar.glm.ldm.Constants.Verb {
+    
+  }
+
+  public interface Preposition extends org.cougaar.glm.ldm.Constants.Preposition {
+    String READYFORTRANSPORT       = "ReadyForTransport";       // For RequestForCapability
+  }
+
+
+  public static class Role extends org.cougaar.glm.ldm.Constants.Role {
+    /**
+     * Insure that Role constants are initialized. Actually does
+     * nothing, but the classloader insures that all static
+     * initializers have been run before executing any code in this
+     * class.
+     **/
+    public static void init() {
+    }
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ bbn/src/org/cougaar/logistics/ldm/LogisticsDomain.java
@@ -0,0 +1,160 @@
+/*
+ * <copyright>
+ *  Copyright 1997-2003 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.logistics.ldm;
+
+import java.util.*;
+
+import org.cougaar.core.mts.MessageAddress;
+import org.cougaar.core.service.AgentIdentificationService;
+import org.cougaar.core.service.DomainService;
+
+import org.cougaar.core.domain.*;
+import org.cougaar.core.component.BindingSite;
+import org.cougaar.core.component.ServiceBroker;
+
+import org.cougaar.core.domain.DomainAdapter;
+
+import org.cougaar.glm.ldm.lps.*;
+import org.cougaar.glm.ldm.plan.AlpineAspectType;
+import org.cougaar.logistics.ldm.asset.PropertyGroupFactory;
+
+import org.cougaar.planning.ldm.LDMServesPlugin;
+import org.cougaar.planning.ldm.PlanningFactory;
+import org.cougaar.planning.service.LDMService;
+
+
+/**
+ * COUGAAR Domain package definition.
+ **/
+
+public class LogisticsDomain extends DomainAdapter {
+
+  public static final String LOGISTICS_NAME = "logistics";
+
+  private MessageAddress self;
+  private AgentIdentificationService agentIdService;
+  private DomainService domainService;
+  private LDMService ldmService;
+
+  public String getDomainName() {
+    return LOGISTICS_NAME;
+  }
+
+  public LogisticsDomain() {
+    super();
+  }
+
+  public void setAgentIdentificationService(AgentIdentificationService ais) {
+    this.agentIdService = ais;
+    if (ais == null) {
+      // Revocation
+    } else {
+      this.self = ais.getMessageAddress();
+    }
+  }
+
+  public void setDomainService(DomainService domainService) {
+    this.domainService = domainService;
+  }
+
+  public void setLDMService(LDMService ldmService) {
+    this.ldmService = ldmService;
+  }
+
+  public void initialize() {
+    super.initialize();
+    Constants.Role.init();    // Insure that our Role constants are initted
+  }
+
+  public void unload() {
+    ServiceBroker sb = getBindingSite().getServiceBroker();
+    if (ldmService != null) {
+      sb.releaseService(
+          this, LDMService.class, ldmService);
+      ldmService = null;
+    }
+    if (domainService != null) {
+      sb.releaseService(
+          this, DomainService.class, domainService);
+      domainService = null;
+    }
+    if (agentIdService != null) {
+      sb.releaseService(
+          this, AgentIdentificationService.class, agentIdService);
+      agentIdService = null;
+    }
+    super.unload();
+  }
+
+  public Collection getAliases() {
+    ArrayList l = new ArrayList(3);
+    l.add("logistics");
+    l.add("albbn");
+    return l;
+  }
+
+  protected void loadFactory() {
+    LDMServesPlugin ldm = ldmService.getLDM();
+    PlanningFactory ldmf = (PlanningFactory) ldm.getFactory("planning");
+    if (ldmf == null) {
+      throw new RuntimeException("Missing \"planning\" factory!");
+    }
+
+    ldmf.addPropertyGroupFactory(new PropertyGroupFactory());
+
+    //TODO: MWD Remove these lines below?
+    //ldmf.addAssetFactory(new AssetFactory());
+    //Factory f = new GLMFactory();
+    //setFactory(f);
+  }
+
+  protected void loadXPlan() {
+    // no logistics-specific plan
+  }
+
+  protected void loadLPs() {
+    RootPlan rootplan = (RootPlan) getXPlanForDomain("root");
+    if (rootplan == null) {
+      throw new RuntimeException("Missing \"root\" plan!");
+    }
+
+    PlanningFactory ldmf = (PlanningFactory) 
+      domainService.getFactory("planning");
+    if (ldmf == null) {
+      throw new RuntimeException("Missing \"planning\" factory!");
+    }
+
+    /**
+     * We have no new logistics LPs for the time being.
+     *
+    GLMFactory glmFactory = (GLMFactory) 
+      domainService.getFactory("glm");
+
+    addLogicProvider(new ReceiveTransferableLP(rootplan, ldmf));
+    addLogicProvider(new TransferableLP(rootplan, self, ldmf));
+    addLogicProvider(new DetailRequestLP(rootplan, self, glmFactory));
+    addLogicProvider(new OPlanWatcherLP(rootplan, ldmf));
+    */
+
+  }
+
+}
