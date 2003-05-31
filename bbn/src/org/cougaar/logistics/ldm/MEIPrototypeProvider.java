4ac398a3038dbb01a9b1befca819d80fa3e96743
lgoldsto <lgoldsto>
2003-05-31 20:11:44 +0000

    Added handle Level2MEI assets.

--- bbn/src/org/cougaar/logistics/ldm/MEIPrototypeProvider.java
+++ bbn/src/org/cougaar/logistics/ldm/MEIPrototypeProvider.java
@@ -2,11 +2,11 @@
  * <copyright>
  *  Copyright 1997-2003 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
  *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
- * 
+ *
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
  *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
@@ -35,13 +35,18 @@ import org.cougaar.logistics.plugin.inventory.TaskUtils;
 import org.cougaar.logistics.plugin.inventory.TimeUtils;
 import org.cougaar.logistics.plugin.inventory.UtilsProvider;
 import org.cougaar.logistics.plugin.utils.ScheduleUtils;
+import org.cougaar.planning.ldm.plan.NewSchedule;
+import org.cougaar.planning.ldm.plan.NewRoleSchedule;
 import org.cougaar.planning.ldm.asset.AggregateAsset;
 import org.cougaar.planning.ldm.asset.Asset;
 import org.cougaar.planning.ldm.asset.TypeIdentificationPG;
+import org.cougaar.planning.ldm.PlanningFactory;
 import org.cougaar.util.UnaryPredicate;
 
 import java.text.StringCharacterIterator;
 import java.util.Collection;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
@@ -74,12 +79,14 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
   private AssetUtils assetUtils;
   private ScheduleUtils scheduleUtils;
   private BlackboardQueryService queryService;
+  private PlanningFactory factory;
+  private boolean publishedLevel2MeiAsset = false;
 
   private static UnaryPredicate orgsPredicate = new UnaryPredicate() {
     public boolean execute (Object o) {
       if (o instanceof Organization) {
         return ((Organization)o).isSelf();
-      } 
+      }
       return false;
     }
   };
@@ -98,12 +105,18 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
         }
       }
       return false;
-    } 
+    }
+  };
+
+  private static UnaryPredicate level2MeiPredicate = new UnaryPredicate() {
+    public boolean execute(Object o) {
+      return (o instanceof Level2MEIAsset);
+    }
   };
 
   public TaskUtils      getTaskUtils() {return taskUtils;}
   public TimeUtils      getTimeUtils() {return timeUtils;}
-  public AssetUtils     getAssetUtils() {return assetUtils;}  
+  public AssetUtils     getAssetUtils() {return assetUtils;}
   public ScheduleUtils  getScheduleUtils() {return scheduleUtils;}
 
   public LoggingService getLoggingService(Object requestor) {
@@ -118,6 +131,12 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
                                                             null);
   }
 
+  private PlanningFactory getPlanningFactory() {
+    PlanningFactory rootFactory = null;
+    rootFactory = (PlanningFactory) getFactory("planning");
+    return rootFactory;
+  }
+
   public void load() {
     super.load();
     serviceBroker = getBindingSite().getServiceBroker();
@@ -128,6 +147,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
       taskUtils = new TaskUtils(this);
       scheduleUtils = new ScheduleUtils(this);
       queryService = getBlackboardQueryService(this);
+      factory = getPlanningFactory();
       if (queryService == null) {
         logger.error("Unable to get query service");
       }
@@ -184,7 +204,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
 //     System.out.println("Execute called for "+getAgentIdentifier());
     if (!configured) {
       configure();
-    } 
+    }
     if (!meiSubscription.isEmpty()) {
 //       System.out.println("Calling addConsumerPGs() for "+getAgentIdentifier());
       addConsumerPGs(meiSubscription);
@@ -194,12 +214,21 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
   protected void configure() {
 //     System.out.println(" configuring MEIPrototypeProvider for "+getAgentIdentifier());
     Iterator new_orgs = queryService.query(orgsPredicate).iterator();
-    if (new_orgs.hasNext()) { 
+    if (new_orgs.hasNext()) {
       myOrg = (Organization) new_orgs.next();
       Service srvc = myOrg.getOrganizationPG().getService();
       if (srvc != null) {
         service= srvc.toString();
 //         System.out.println("MEIPrototypeProvider configured for "+getAgentIdentifier());
+         if (!publishedLevel2MeiAsset && !queryLevel2Mei()) {
+          Asset asset = factory.createPrototype(org.cougaar.logistics.ldm.asset.Level2MEIAsset.class,
+                                                "Level2MEI");
+          Asset a = factory.createInstance(asset);
+          setupAvailableSchedule(a);
+          logger.debug("MEIPrototypeProvider publishing Level2MEI asset in agent: " + getAgentIdentifier());
+          publishAdd(a);
+          publishedLevel2MeiAsset = true;
+        }
         configured = true;
       } else {
         logger.error("Organization has no Service :"+
@@ -208,40 +237,64 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
     }
   }
 
+  // In case we rehydrate, query the blackboard
+  private boolean queryLevel2Mei() {
+    Collection container = queryService.query(level2MeiPredicate);
+    Object o = null;
+    if (!container.isEmpty()) {
+      o = container.iterator().next();
+      if (o instanceof Level2MEIAsset) {
+        // set this so that we don't keep querying
+        publishedLevel2MeiAsset = true;
+        return true;
+      }
+    }
+    return false;
+  }
 
-    protected  boolean[] checkMeiConsumption(Asset asset) {
-      String type_id = asset.getTypeIdentificationPG().getTypeIdentification();
-      String query = (String) fileParameters_.get ("MeiConsumption");
-      String consumer_id = type_id.substring(type_id.indexOf("/")+1);
-      query = substituteNSN (query, consumer_id);
+  protected  boolean[] checkMeiConsumption(Asset asset) {
+    // The default is that the MEI consumes all 4 classes.
+    // This prevents problems if the table hasn't been updated for
+    // a new MEI.
+    boolean result[] = {true, true, true, true };
 
-      Vector qresult;
-      // The default is that the MEI consumes all 4 classes.
-      // This prevents problems if the table hasn't been updated for
-      // a new MEI.
-      boolean result[] = {true, true, true, true };
-      try {
-        qresult = executeQuery (query);
-        logger.debug ("in checkMeiConsumption() query complete for asset "+
-                      asset.getTypeIdentificationPG().getNomenclature()+
-                      "\nquery= "+query);
-        if (qresult.isEmpty()) {
-          logger.debug ("no result for asset " +
-                        asset.getTypeIdentificationPG().getNomenclature());
-        } else {
-          Object row[] = (Object[])qresult.firstElement();
-          result[AMMO]=convertConsumptionElement(row[AMMO]);
-          result[PKG_POL]=convertConsumptionElement(row[PKG_POL]);
-          result[SPARES]=convertConsumptionElement(row[SPARES]);
-          result[FUEL]=convertConsumptionElement(row[FUEL]);
-        } // if else
-      } catch (Exception ee) {
-        logger.error ( "in checkMeiConsumption(), DB query failed.Query= "+query);
-
-      } // try
-
+    if (asset instanceof Level2MEIAsset)   {
+      result[AMMO] = true;
+      result[PKG_POL]= false;
+      result[SPARES]= false;
+      result[FUEL]= false;
       return result;
     }
+
+    String type_id = asset.getTypeIdentificationPG().getTypeIdentification();
+    String query = (String) fileParameters_.get ("MeiConsumption");
+    String consumer_id = type_id.substring(type_id.indexOf("/")+1);
+    query = substituteNSN (query, consumer_id);
+
+    Vector qresult;
+
+    try {
+      qresult = executeQuery (query);
+      logger.debug ("in checkMeiConsumption() query complete for asset "+
+                    asset.getTypeIdentificationPG().getNomenclature()+
+                    "\nquery= "+query);
+      if (qresult.isEmpty()) {
+        logger.debug ("no result for asset " +
+                      asset.getTypeIdentificationPG().getNomenclature());
+      } else {
+        Object row[] = (Object[])qresult.firstElement();
+        result[AMMO]=convertConsumptionElement(row[AMMO]);
+        result[PKG_POL]=convertConsumptionElement(row[PKG_POL]);
+        result[SPARES]=convertConsumptionElement(row[SPARES]);
+        result[FUEL]=convertConsumptionElement(row[FUEL]);
+      } // if else
+    } catch (Exception ee) {
+      logger.error ( "in checkMeiConsumption(), DB query failed.Query= "+query);
+
+    } // try
+
+    return result;
+  }
 
   private boolean convertConsumptionElement(Object ele) {
     boolean res = true;
@@ -292,7 +345,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
           anAsset.setPropertyGroup(fuelpg);
           addedPG = true;
         }
-        if (consumes(consumed, AMMO) && anAsset.searchForPropertyGroup(AmmoConsumerPG.class) == null) {        
+        if (consumes(consumed, AMMO) && anAsset.searchForPropertyGroup(AmmoConsumerPG.class) == null) {
           logger.debug("addConsumerPGs() CREATING AmmoConsumerPG for "+anAsset+" in "+
                        getAgentIdentifier());
 
@@ -301,7 +354,12 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
           ammopg.setMei(a);
           ammopg.setService(service);
           ammopg.setTheater(THEATER);
-          ammopg.setAmmoBG(new AmmoConsumerBG(ammopg));
+          if (anAsset instanceof Level2MEIAsset) {
+
+            ammopg.setAmmoBG(new Level2AmmoConsumerBG(ammopg));
+          } else {
+            ammopg.setAmmoBG(new AmmoConsumerBG(ammopg));
+          }
           ammopg.initialize(this);
           anAsset.setPropertyGroup(ammopg);
           addedPG = true;
@@ -337,7 +395,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
         if (addedPG) {
           publishChange(a);
         }
-      } 
+      }
     }
   }
 
@@ -369,7 +427,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
 
 
   public Asset makePrototype (String type_name, Class class_hint) {
-    // Demand Rate Queries are based upon service 
+    // Demand Rate Queries are based upon service
     if (!configured) {
       configure();
       if (!configured) {
@@ -464,7 +522,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
       consumer_id = getNormalizedName(consumer_id);
     } // if
     Vector result = null;
-    String nomen = null; 
+    String nomen = null;
     if (query != null) {
       int i = query.indexOf(":nsn");
       String q1 = query.substring(0,i) + "'"+consumer_id+"'";
@@ -483,8 +541,8 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
         logger.error("retrieveFromDB(), DB query failed. query= "+query+
                      "\n ERROR "+ee);
         return null;
-      } 
-    } 
+      }
+    }
     return nomen;
   } // getMEINomenclature
 
@@ -575,7 +633,7 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
         } // if
       } // if
     } // if
-    //System.JTEST.out.println ("The string AFTER the substitution was " + query);  		
+    //System.JTEST.out.println ("The string AFTER the substitution was " + query);
     return query;
   } // substituteNSN
 
@@ -625,6 +683,18 @@ public class MEIPrototypeProvider extends QueryLDMPlugin implements UtilsProvide
     return null;
   } // substituteNSNList
 
+  private void setupAvailableSchedule(Asset asset) {
+    Calendar mycalendar = Calendar.getInstance();
+    // set the start date of the available schedule to 01/01/1990
+    mycalendar.set(1990, 0, 1, 0, 0, 0);
+    Date start = mycalendar.getTime();
+    // set the end date of the available schedule to 01/01/2010
+    mycalendar.set(2010, 0, 1, 0, 0, 0);
+    Date end = mycalendar.getTime();
+    NewSchedule availsched = factory.newSimpleSchedule(start, end);
+    // set the available schedule
+    ((NewRoleSchedule)asset.getRoleSchedule()).setAvailableSchedule(availsched);
+  }
 } // MEIPrototypeProvider
 
 
