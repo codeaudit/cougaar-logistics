a995a57425eff5e7995b17db25e90de676c65af1
lgoldsto <lgoldsto>
2003-07-18 16:02:59 +0000

    Cleaned up imports.

--- bbn/src/org/cougaar/logistics/plugin/packer/ALAmmoPackerPredicate.java
+++ bbn/src/org/cougaar/logistics/plugin/packer/ALAmmoPackerPredicate.java
@@ -2,11 +2,11 @@
  * <copyright>
  *  Copyright 2003 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
  *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
- * 
+ *
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
  *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
@@ -18,55 +18,49 @@
  *  PERFORMANCE OF THE COUGAAR SOFTWARE.
  * </copyright>
  */
-package org.cougaar.logistics.plugin.packer; 
+package org.cougaar.logistics.plugin.packer;
 
+import org.cougaar.glm.ldm.asset.Ammunition;
+import org.cougaar.logistics.ldm.Constants;
+import org.cougaar.planning.ldm.plan.PlanElement;
+import org.cougaar.planning.ldm.plan.Task;
 import org.cougaar.util.UnaryPredicate;
 
-import org.cougaar.planning.ldm.plan.PlanElement;
-import org.cougaar.planning.ldm.plan.Preposition;
-import org.cougaar.planning.ldm.plan.Task;
-import org.cougaar.planning.ldm.plan.Verb;
-import org.cougaar.planning.ldm.plan.Preposition;
- 
-import org.cougaar.logistics.ldm.Constants;
-import org.cougaar.glm.ldm.asset.Ammunition;
-import org.cougaar.glm.packer.GenericPlugin;
-
 /**
-  * This UnaryPredicate is used to test whether Tasks should be
-  * packed together, in the Packer's packing rule.  It picks out
-  * all supply tasks that request Ammunition.
-  */
+ * This UnaryPredicate is used to test whether Tasks should be
+ * packed together, in the Packer's packing rule.  It picks out
+ * all supply tasks that request Ammunition.
+ */
 
-public class ALAmmoPackerPredicate  {
+public class ALAmmoPackerPredicate {
 
   public static UnaryPredicate getInputTaskPredicate() {
     return new UnaryPredicate() {
-      
+
       public boolean execute(Object o) {
         if ((o instanceof Task) &&
-            (((Task)o).getVerb().equals(Constants.Verb.SUPPLY)) &&
-            (((Task)o).getPrepositionalPhrase(GenericPlugin.INTERNAL) == null) &&
-            (((Task)o).getDirectObject() instanceof Ammunition) &&
-            (((Task)o).getPrepositionalPhrase("LOW_FIDELITY") == null)) {// BOZO - change this to a const reference later...
+            (((Task) o).getVerb().equals(Constants.Verb.SUPPLY)) &&
+            (((Task) o).getPrepositionalPhrase(GenericPlugin.INTERNAL) == null) &&
+            (((Task) o).getDirectObject() instanceof Ammunition) &&
+            (((Task) o).getPrepositionalPhrase("LOW_FIDELITY") == null)) {// BOZO - change this to a const reference later...
           return true;
         } else {
-	  /*if ((o instanceof Task) &&
-            (((Task)o).getVerb().equals(Constants.Verb.SUPPLY))) {
-            if (((Task)o).getPrepositionalPhrase(GenericPlugin.INTERNAL) != null)
-            System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() + 
-            " b/c has internal prep.");
-	    else if (!(((Task)o).getDirectObject() instanceof Ammunition))
-            System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() + 
-            " b/c d.o. not Ammunition.");
-	    else if (!(((Task)o).getPrepositionalPhrase("LOW_FIDELITY") != null))
-            System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() + 
-            " b/c has LOW_FIDELITY prep.");
-	    else
-            System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() + 
-            " b/c - ?");
-            
-            } */
+          /*if ((o instanceof Task) &&
+(((Task)o).getVerb().equals(Constants.Verb.SUPPLY))) {
+if (((Task)o).getPrepositionalPhrase(GenericPlugin.INTERNAL) != null)
+System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() +
+" b/c has internal prep.");
+            else if (!(((Task)o).getDirectObject() instanceof Ammunition))
+System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() +
+" b/c d.o. not Ammunition.");
+            else if (!(((Task)o).getPrepositionalPhrase("LOW_FIDELITY") != null))
+System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() +
+" b/c has LOW_FIDELITY prep.");
+            else
+System.out.println ("AmmoPackerPredicate - Ignoring supply task " + ((Task)o).getUID() +
+" b/c - ?");
+
+} */
           return false;
         }
       }
@@ -79,10 +73,10 @@ public class ALAmmoPackerPredicate  {
 
       public boolean execute(Object o) {
         if (o instanceof PlanElement) {
-          Task task = ((PlanElement)o).getTask();
+          Task task = ((PlanElement) o).getTask();
           if (myInputTaskPredicate.execute(task) ||
-              ((task.getPrepositionalPhrase(GenericPlugin.INTERNAL) !=null) &&
-               (task.getDirectObject() instanceof Ammunition))) {
+              ((task.getPrepositionalPhrase(GenericPlugin.INTERNAL) != null) &&
+              (task.getDirectObject() instanceof Ammunition))) {
             return true;
           } else {
             return false;
