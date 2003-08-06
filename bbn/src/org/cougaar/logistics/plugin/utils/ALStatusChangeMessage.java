2d77c8d0fa8fb91f3c3472ed1a4cd4110d5f9aca
aleung <aleung>
2003-08-06 17:06:00 +0000

    change to extend service discovery status change message

--- bbn/src/org/cougaar/logistics/plugin/utils/ALStatusChangeMessage.java
+++ bbn/src/org/cougaar/logistics/plugin/utils/ALStatusChangeMessage.java
@@ -3,11 +3,11 @@
  *  Copyright 2003 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA)
  *  and the Defense Logistics Agency (DLA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
  *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
- * 
+ *
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
  *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
@@ -22,34 +22,20 @@
 package org.cougaar.logistics.plugin.utils;
 
 import java.util.Date;
+import org.cougaar.servicediscovery.description.StatusChangeMessage;
 
 /**
  *
- * The object that the SD Use Case Servlet will publish to 
+ * The object that the SD Use Case Servlet will publish to
  * indicate the change in role of a provider.
  *
  */
-public class ALStatusChangeMessage {
-  private String role;
-  private boolean registryUpdated;
+public class ALStatusChangeMessage extends StatusChangeMessage{
   private Date endDate;
 
   public ALStatusChangeMessage(String role, boolean registryUpdated, Date endDate) {
-    this.role = role;
-    this.registryUpdated = registryUpdated;
+    super(role, registryUpdated);
     this.endDate = endDate;
-  }
-
-  public String getRole() {
-    return role;
-  }
-
-  public void setRegistryUpdated(boolean registryUpdated) {
-    this.registryUpdated = registryUpdated;
-  }
-
-  public boolean registryUpdated() {
-    return registryUpdated;
   }
 
   public Date getEndDate() {
