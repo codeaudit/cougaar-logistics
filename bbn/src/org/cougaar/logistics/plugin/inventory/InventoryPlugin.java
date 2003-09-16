833d12120557f755c658a64252df03acb88af19e
lgoldsto <lgoldsto>
2003-09-16 18:04:23 +0000

    Minor reformat change.

--- bbn/src/org/cougaar/logistics/plugin/inventory/InventoryPlugin.java
+++ bbn/src/org/cougaar/logistics/plugin/inventory/InventoryPlugin.java
@@ -1,22 +1,22 @@
 /*
-* <copyright>
-*  Copyright 1997-2003 BBNT Solutions, LLC
-*  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
-*
-*  This program is free software; you can redistribute it and/or modify
-*  it under the terms of the Cougaar Open Source License as published by
-*  DARPA on the Cougaar Open Source Website (www.cougaar.org).
-*
-*  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
-*  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
-*  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
-*  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
-*  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
-*  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
-*  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
-*  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-*  PERFORMANCE OF THE COUGAAR SOFTWARE.
-* </copyright>
+ * <copyright>
+ *  Copyright 1997-2003 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
 */
 
 package org.cougaar.logistics.plugin.inventory;
@@ -409,7 +409,7 @@ public class InventoryPlugin extends ComponentPlugin
       // but only if we are not about to rip out previous work we have done
       if (didOrgRelationshipsChange()) {
 //        logger.warn("ORG RELATIONSHIPS CHANGED");
-//	  System.out.println("SDSD myorg: " + myOrganization + " supply type:" + 
+//	  System.out.println("SDSD myorg: " + myOrganization + " supply type:" +
 //			       supplyType + " role: " + getRole(supplyType) + "\n");
         HashMap providers = getProvidersAndEndDates();
         Collection unprovidedTasks = getUnprovidedTasks(refillAllocationSubscription,
@@ -435,7 +435,7 @@ public class InventoryPlugin extends ComponentPlugin
         if (addedSupply.isEmpty() && changedSupply.isEmpty()) {
           sortIncomingSupplyTasks(getTaskUtils().getUnallocatedTasks(nonrefillSubscription,
                                                                      Constants.Verb.Supply));
-//             unalloc = sortIncomingSupplyTasks(getTaskUtils().getUnallocatedTasks(nonrefillSubscription, 
+//             unalloc = sortIncomingSupplyTasks(getTaskUtils().getUnallocatedTasks(nonrefillSubscription,
 // 										       Constants.Verb.Supply));
 //             if (!unalloc.isEmpty()){
 // 	      if (logger.isWarnEnabled())
@@ -568,7 +568,7 @@ public class InventoryPlugin extends ComponentPlugin
                                                                  TimeSpan.MAX_VALUE);
     Iterator rit = relationships.iterator();
     Role myRole = getRole(supplyType);
-// 	  System.out.println("SDSD myorg: " + myOrganization + " supply type:" + 
+// 	  System.out.println("SDSD myorg: " + myOrganization + " supply type:" +
 // 			       supplyType + " role: " + getRole(supplyType) + "\n");
     while (rit.hasNext()) {
       Relationship r = (Relationship) rit.next();
@@ -610,7 +610,7 @@ public class InventoryPlugin extends ComponentPlugin
       }
     }
 //       if (! unprovidedTasks.isEmpty()) {
-// 	  System.out.println("SDSD unprovided: " + unprovidedTasks + "\n");	
+// 	  System.out.println("SDSD unprovided: " + unprovidedTasks + "\n");
 //       }
     return unprovidedTasks;
   }
