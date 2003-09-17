2fb371aa55a1387b85214fd016c36190986aa131
bdepass <bdepass>
2003-09-17 23:34:57 +0000

    Insert copyright headers

--- bbn/src/org/cougaar/logistics/plugin/servicediscovery/ALProviderPlugin.java
+++ bbn/src/org/cougaar/logistics/plugin/servicediscovery/ALProviderPlugin.java
@@ -1,3 +1,25 @@
+/*
+ * <copyright>
+ *  Copyright 2002-2003 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA)
+ *  and the Defense Logistics Agency (DLA).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ *
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
 package org.cougaar.logistics.plugin.servicediscovery;
 
 import org.cougaar.servicediscovery.SDFactory;
@@ -24,8 +46,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * <p>Title: </p>
- * <p>Description:
  * This plugin replies to new service contract relays. Usually,
  * it will reply with a service contract which exactly matches the
  * service request. "Just say yes"
@@ -37,11 +57,6 @@ import java.util.Date;
  * Also, the plugin will now reply to any new service contract relay with
  * a service contract with start time == end time (a zero time contract)
  * as a way to "Just say no".
- * </p>
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: </p>
- * @author unascribed
- * @version 1.0
  */
 
 public class ALProviderPlugin extends SDProviderPlugin {
