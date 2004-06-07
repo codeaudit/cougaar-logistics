93569798dceafb11e223e73106b2af3a99dba286
lgoldsto <lgoldsto>
2004-06-07 16:34:40 +0000

    Added debugging and removed some commented out code.  Check success on publishing to the blackboard.

--- bbn/src/org/cougaar/logistics/plugin/inventory/ExternalAllocator.java
+++ bbn/src/org/cougaar/logistics/plugin/inventory/ExternalAllocator.java
@@ -54,154 +54,156 @@ import org.cougaar.core.component.ServiceBroker;
 
 public class ExternalAllocator extends InventoryModule implements AllocatorModule {
 
-    Role         providerRole;
-    /** list of nsn's with no resupply point */
-    private Vector noResupply;
+  Role providerRole;
+  /** list of nsn's with no resupply point */
+  private Vector noResupply;
 
+  public ExternalAllocator(InventoryPlugin imPlugin, Role provider) {
+    super(imPlugin);
+    providerRole = provider;
+    noResupply = new Vector();
+  }
 
-    public ExternalAllocator(InventoryPlugin imPlugin, Role provider) {
-	super(imPlugin);
-	providerRole = provider;
-	noResupply = new Vector();
+  public void forwardSupplyTasks(Collection supplyTasks) {
+    //assuming inventory plugin calls this with knowledge that 
+    //all passed tasks are to be forwarded
+    Task supplyTask;
+    Iterator tasksIter = supplyTasks.iterator();
+    if (providerRole == null) {
+      logger.error("Unknown Role.  Cannot allocate tasks.");
+      return;
     }
-
-    public void forwardSupplyTasks(Collection supplyTasks) {
-	//assuming inventory plugin calls this with knowledge that 
-	//all passed tasks are to be forwarded
-	Task supplyTask;
-	Iterator tasksIter = supplyTasks.iterator();
-	if (providerRole == null) {
-	  logger.error("Unknown Role.  Cannot allocate tasks.");
-	  return;
-	}
-	while (tasksIter.hasNext()) {
-	    supplyTask = (Task)tasksIter.next();
-	    allocateTask(supplyTask);
-	}
+    while (tasksIter.hasNext()) {
+      supplyTask = (Task)tasksIter.next();
+      allocateTask(supplyTask);
     }
+  }
 
   // allocate our new refill tasks
-    public void allocateRefillTasks(Collection newRefills) {
-	Task refill;
-	Iterator refillIT = newRefills.iterator();
-	while(refillIT.hasNext()) {
-	    refill = (Task) refillIT.next();
-	    allocateTask(refill);
-	}
-    }
-
-    private boolean allocateTask(Task task) {
-      if (task.getPlanElement() != null) {
-        logger.error("Found Task with PlanElement. Ignoring task.");
-        return true;
+  public void allocateRefillTasks(Collection newRefills) {
+    Task refill;
+    Iterator refillIT = newRefills.iterator();
+    while(refillIT.hasNext()) {
+      refill = (Task) refillIT.next();
+      if (logger.isDebugEnabled()) {
+        logger.debug("Agent: " + inventoryPlugin.getClusterId().toString() + "ExtAlloc" + inventoryPlugin.getSupplyType()+"]" +
+                     "trying to allocate refill task: " + refill.getUID());
       }
-	Organization provider = findBestSource(task);
-	if (provider != null) {
-	  if(verifyBeforeAllocation(task,provider)){
-	    AllocationResult estAR =  createPredictedAllocationResult(task);
-            Allocation alloc;
-            //either make an allocation or reset the estimated AR on the
-            // already existing pe - mostly likely to happen if a task's prefs
-            // changed.
-            if (task.getPlanElement() == null) {
-              alloc = buildAllocation(task, provider, providerRole);
-              if (estAR != null){
-                alloc.setEstimatedResult(estAR);
-                if(inventoryPlugin.publishAdd(alloc)) {
-                  return true;
-                }
-                else {
-                  logger.error("Unable to publish the allocation " + alloc);
-                }
+      allocateTask(refill);
+    }
+  }
+
+  private boolean allocateTask(Task task) {
+    if (task.getPlanElement() != null) {
+      logger.error("Found Task with PlanElement. Ignoring task.");
+      return true;
+    }
+    Organization provider = findBestSource(task);
+    if (provider != null) {
+      if(verifyBeforeAllocation(task,provider)){
+        AllocationResult estAR =  createPredictedAllocationResult(task);
+        Allocation alloc;
+        //either make an allocation or reset the estimated AR on the
+        // already existing pe - mostly likely to happen if a task's prefs
+        // changed.
+        if (task.getPlanElement() == null) {
+          alloc = buildAllocation(task, provider, providerRole);
+          if (estAR != null){
+            alloc.setEstimatedResult(estAR);
+            if(inventoryPlugin.publishAdd(alloc)) {
+              if (logger.isDebugEnabled()) {
+                logger.debug("Agent: " + inventoryPlugin.getClusterId().toString() + "ExtAlloc[" + inventoryPlugin.getSupplyType()+"]" +
+                             "publish Adding allocation: " + alloc.getUID()+
+                             " parent task is: " + task.getUID() + " publishAdd returned: true");
               }
-            } else {
-              alloc = (Allocation)task.getPlanElement();
-              if (estAR != null){
-                alloc.setEstimatedResult(estAR);
-                if(inventoryPlugin.publishChange(alloc)) {
-                  return true;
-                }
-                else {
-                  logger.error("Unable to publish Change the allocation " + alloc);
-                }
-              }
+              return true;
             }
-          } 
-        }
-        return false;
-    }
-
-    /** build Allocation with an estimated alloc result */
-    private Allocation buildAllocation(Task t, Asset a, Role r)
-    {
-	return inventoryPlugin.getPlanningFactory().createAllocation(t.getPlan(), t, a, null, r);
-    }
-
-    private AllocationResult createPredictedAllocationResult(Task task) {
-	//MWD in the future this will have to generate the expected result
-	//from the predictor.
-	return new AllocationResultHelper(task, null).getAllocationResult(0.25, true);
-	// return PluginHelper.createEstimatedAllocationResult(task, 
-// 							    inventoryPlugin.getPlanningFactory(), 
-// 							    0.25, true);
-    }
-
-    /** Figure out which organization supplying item is best for us. */
-    private Organization findBestSource(Task task) {
-	Enumeration support_orgs;
-        if (getTaskUtils().isProjection(task)) {
-            /* For a projection, should be time-phased as support
-               changes over time. We ignore that, for now */
-            support_orgs = getAssetUtils().getSupportingOrgs(inventoryPlugin.getMyOrganization(), 
-							     providerRole, 
-							     getTaskUtils().getStartTime(task), 
-							     TaskUtils.getEndTime(task));
+            else {
+              logger.error("Unable to publish the allocation " + alloc);
+            }
+          }
         } else {
-            support_orgs = getAssetUtils().getSupportingOrgs(inventoryPlugin.getMyOrganization(), 
-                                                             providerRole, 
-                                                             getTaskUtils().getEndTime(task));
+          alloc = (Allocation)task.getPlanElement();
+          if (estAR != null){
+            alloc.setEstimatedResult(estAR);
+            if(inventoryPlugin.publishChange(alloc)) {
+              return true;
+            }
+            else {
+              logger.error("Unable to publish Change the allocation " + alloc);
+            }
+          }
         }
-	if (support_orgs.hasMoreElements()) {
-	    // For now, returning the first supporting org during the time span
-	    return (Organization)support_orgs.nextElement();
-	}
-	else {
-	    String itemId = task.getDirectObject().getTypeIdentificationPG().getTypeIdentification();
-	    if (getTaskUtils().isProjection(task)) {
-		logger.warn("No "+providerRole+" for task " + task.getUID() + ", during ["+
-                            getTimeUtils().dateString(getTaskUtils().getStartTime(task))+
-			     "-" +
-			   getTimeUtils().dateString(TaskUtils.getEndTime(task)) +"]"+". Will retry.");
-	    } else {
-		logger.warn("No "+providerRole+" for task " + task.getUID() + ", during "+
-                            getTimeUtils().dateString(getTaskUtils().getEndTime(task))+". Will retry.");
-	    }
-	    // prevents same error message from being printed many times.
+      }
+    }
+    return false;
+  }
+
+  /** build Allocation with an estimated alloc result */
+  private Allocation buildAllocation(Task t, Asset a, Role r)
+  {
+    return inventoryPlugin.getPlanningFactory().createAllocation(t.getPlan(), t, a, null, r);
+  }
+
+  private AllocationResult createPredictedAllocationResult(Task task) {
+    //MWD in the future this will have to generate the expected result
+    //from the predictor.
+    return new AllocationResultHelper(task, null).getAllocationResult(0.25, true);
+  }
+
+  /** Figure out which organization supplying item is best for us. */
+  private Organization findBestSource(Task task) {
+    Enumeration support_orgs;
+    if (getTaskUtils().isProjection(task)) {
+      /* For a projection, should be time-phased as support
+      changes over time. We ignore that, for now */
+      support_orgs = getAssetUtils().getSupportingOrgs(inventoryPlugin.getMyOrganization(),
+                                                       providerRole,
+                                                       getTaskUtils().getStartTime(task),
+                                                       TaskUtils.getEndTime(task));
+    } else {
+      support_orgs = getAssetUtils().getSupportingOrgs(inventoryPlugin.getMyOrganization(),
+                                                       providerRole,
+                                                       getTaskUtils().getEndTime(task));
+    }
+    if (support_orgs.hasMoreElements()) {
+      // For now, returning the first supporting org during the time span
+      return (Organization)support_orgs.nextElement();
+    }
+    else {
+      String itemId = task.getDirectObject().getTypeIdentificationPG().getTypeIdentification();
+      if (getTaskUtils().isProjection(task)) {
+        logger.warn("No "+providerRole+" for task " + task.getUID() + ", during ["+
+                    getTimeUtils().dateString(getTaskUtils().getStartTime(task))+
+                    "-" +
+                    getTimeUtils().dateString(TaskUtils.getEndTime(task)) +"]"+". Will retry.");
+      } else {
+        logger.warn("No "+providerRole+" for task " + task.getUID() + ", during "+
+                    getTimeUtils().dateString(getTaskUtils().getEndTime(task))+". Will retry.");
+      }
+      // prevents same error message from being printed many times.
 // 	    if (!noResupply.contains(itemId)) {
 // 		logger.debug("findBestSource() <"+ providerRole +
 // 			   "> allocateRefillTask no best source for "+itemId);
 // 		noResupply.addElement(itemId);
 // 	    }
-	}
-	return null;
     }
+    return null;
+  }
 
-
-
-    private boolean verifyBeforeAllocation(Task task, Organization org) {
-      // Do not allocate tasks after they have taken place-AF does this make sense?
-      if (!(task.beforeCommitment(new Date(inventoryPlugin.currentTimeMillis())))) {
-	logger.warn("verifyBeforeAllocation: return ... after commitment"+task.getCommitmentDate()+" task:"+task+" to Asset "+org);
-	// too late to change
-	return false;
-      }
-      return true;
+  private boolean verifyBeforeAllocation(Task task, Organization org) {
+    // Do not allocate tasks after they have taken place-AF does this make sense?
+    if (!(task.beforeCommitment(new Date(inventoryPlugin.currentTimeMillis())))) {
+      logger.warn("verifyBeforeAllocation: return ... after commitment"+task.getCommitmentDate()+" task:"+task+" to Asset "+org);
+      // too late to change
+      return false;
     }
-
+    return true;
+  }
 
   public HashSet updateAllocationResult(Collection sub) {
     //    System.out.println("ex alloc update ARs being called in" + inventoryPlugin.getClusterId());
-      HashSet backwardFlowInventories = new HashSet();
+    HashSet backwardFlowInventories = new HashSet();
     // Set up the affected inventories for the AllocationAssessor
     Task refill;
     Asset asset;
@@ -214,15 +216,20 @@ public class ExternalAllocator extends InventoryModule implements AllocatorModul
       refill = alloc.getTask();
       asset = (Asset)refill.getDirectObject();
       inventory = inventoryPlugin.findOrMakeInventory(asset);
-        
+
       if (PluginHelper.updatePlanElement(alloc)) {
-        inventoryPlugin.publishChange(alloc);
+        boolean didPubChg = inventoryPlugin.publishChange(alloc);
+        if (logger.isDebugEnabled()) {
+          logger.debug("Agent: " + inventoryPlugin.getClusterId().toString() + "ExtAlloc[" + inventoryPlugin.getSupplyType()+"]" +
+                       "publist Changing allocation due to AR change: " + alloc.getUID()+ 
+                       " parent task is: " + alloc.getTask().getUID() + " publishChange returned: " + didPubChg);
+        }
         if (inventory != null) { // else { this is a pass-thru }
           //System.out.println("ex all publish changing and added to AA's inventory list " + logInvPG.getResource());
           backwardFlowInventories.add(inventory);
 
           logInvPG = (LogisticsInventoryPG)
-            inventory.searchForPropertyGroup(LogisticsInventoryPG.class);
+              inventory.searchForPropertyGroup(LogisticsInventoryPG.class);
           if (refill.getVerb().equals(Constants.Verb.PROJECTSUPPLY)) {
             logInvPG.updateRefillProjection(refill);
           } else {
@@ -231,20 +238,6 @@ public class ExternalAllocator extends InventoryModule implements AllocatorModul
         }
       }
     }
-    //rebuildPGCustomerHash();
     return backwardFlowInventories;
   }
-
-//   private void rebuildPGCustomerHash() {
-//     Collection changedInventories = inventoryPlugin.getTouchedInventories();
-//     Iterator invIter = changedInventories.iterator();
-//     Inventory inventory;
-//     LogisticsInventoryPG thePG;
-//     while (invIter.hasNext()) {
-//       inventory = (Inventory)invIter.next();
-//       thePG = (LogisticsInventoryPG)
-// 	inventory.searchForPropertyGroup(LogisticsInventoryPG.class);
-//       thePG.rebuildCustomerHash();
-//     }
-//   }
 }
