15c41847662b05da52846608f0358510678a0cdd
jnilsson <jnilsson>
2004-10-11 17:07:48 +0000

    Fixed exception handling

--- bbn/data/csmart/reports/acme_reporting_logistics/core.rb
+++ bbn/data/csmart/reports/acme_reporting_logistics/core.rb
@@ -34,75 +34,19 @@ module ACME
           ShortfallTest.new(archive, @plugin, @ikko).perform
           puts "SHORTFALL"
         rescue
-          puts $!
-          puts $!.backtrace
+          error_str =  "Exception<BR>\n#{Time.now}<BR>\n#{$!.message}<BR>\n#{$!.backtrace.join("<BR>\n")}"
+          puts error_str
+          @plugin.log_error << error_str
           archive.add_report("Exception", @plugin.plugin_configuration.name) do |report|
             report.open_file("Exception.html", "text/html", "Exception") do |out|
               out.puts "<html>"
               out.puts "<title>Exception</title>"
-              out.puts "#{$!} <BR>"
-              out.puts $!.backtrace.collect{|x| x.gsub(/&/, "&amp;").gsub(/</, "&lt;").gsub(/>/,"&gt;")}.join("<BR>")
+              out.puts error_str
               out.puts "</html>"
             end
             report.failure
-          end
+          end          
         end
-      end
-      
-      def run_log_test(archive)
-        archive.add_report("Log", @plugin.plugin_configuration.name) do |report|
-          run_log = nil
-          archive.files_with_name(/run\.log/).each do |f|
-            run_log = f.name
-          end
-          if run_log
-            error = 0
-            run_data = File.readlines(run_log)
-            last_run = []
-            run_data.each do |line|
-              if line =~ /Run:.*started/ then
-              #only care if last run in the log was interrupted
-                error = 0 
-                last_run = []
-              end
-              error = 1 if (line =~ /INTERRUPT/ && error == 0)
-              error = 2 if (line =~ /\[ERROR\]/)
-              last_run << line
-            end
-            run_data = run_data.collect!{|x| x.gsub(/&/, "&amp;").gsub(/</, "&lt;").gsub(/>/,"&gt;")}
-            report.open_file("run_report.html", "text/html", "Full Run Log") do |file|
-              file.puts @ikko['run_report.html', {'description_link'=>"run_report_description.html", 'data'=>last_run}]
-            end
-
-            desc = create_description
-            report.open_file("run_report_description.html", "text/html", "Run Report Description") do |file|
-              file.puts desc
-            end
-
-
-            if (error == 0) then
-              report.success
-            elsif (error == 1) then
-              report.partial_success
-            else
-              report.failure          
-            end
-          else
-            report.failure
-          end
-        end
-      end
-      
-      def create_description
-        ikko_data = {}
-        ikko_data["name"] = "Run Report"
-        ikko_data["title"] = "Run Report Description"
-        ikko_data["description"] = "Prints the listing of the run.log for the current run.  Other runs in the log are ignored"
-        success_table = {"success"=>"No Errors or interrupts in the run",
-                         "partial"=>"Interrupts present but no errors",
-                         "fail"=>"Errors present or run.log file not found"}
-        ikko_data["table"] = @ikko["success_template.html", success_table]
-        return @ikko["description.html", ikko_data]
       end
     end
   end       
