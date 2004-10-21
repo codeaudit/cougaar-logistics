508c8ce1698cb549cdd8fef2f6809f59b4cafd30
mdavis <mdavis>
2004-10-21 16:21:52 +0000

    New data object held by ShortfallInventory objects to represent a period of red inventory shortfall.   This is the new data for the Assessment team new functionality to the ShortfallAlertServlet.   It holds information like start and end of period, shortfall qty and percent shortfall.

---
+++ bbn/src/org/cougaar/logistics/plugin/inventory/ShortfallPeriod.java
@@ -0,0 +1,108 @@
+/* 
+ * <copyright>
+ *  Copyright 2002-2003 BBNT Solutions, LLC
+ *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the Cougaar Open Source License as published by
+ *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
+ * 
+ *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
+ *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
+ *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, AND WITHOUT
+ *  ANY WARRANTIES AS TO NON-INFRINGEMENT.  IN NO EVENT SHALL COPYRIGHT
+ *  HOLDER BE LIABLE FOR ANY DIRECT, SPECIAL, INDIRECT OR CONSEQUENTIAL
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE OF DATA OR PROFITS,
+ *  TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THE COUGAAR SOFTWARE.
+ * </copyright>
+ */
+
+package org.cougaar.logistics.plugin.inventory;
+
+import org.cougaar.core.util.UniqueObject;
+import org.cougaar.core.blackboard.Publishable;
+import org.cougaar.core.util.UID;
+import org.cougaar.planning.servlet.data.xml.XMLWriter;
+import org.cougaar.planning.servlet.data.xml.XMLable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Date;
+
+/** 
+ * ShortfallSummary is an object that summarizizes which inventories of 
+ * a particular inventory have shortfall.
+ */
+
+public class ShortfallPeriod implements java.io.Serializable, Publishable {
+
+  private long startTime;
+  private long endTime;
+  private double totalShortfallDemand;
+  private double totalFilled;
+
+  /**
+   * Constructor 
+   */
+
+
+  public ShortfallPeriod () {
+  }
+  
+  public ShortfallPeriod (long aStartTime,
+			  long anEndTime) {
+      this(aStartTime,anEndTime,0.0d,0.0d);
+  }
+
+  public ShortfallPeriod (long aStartTime,
+			  long anEndTime,
+			  double demand,
+			  double filled) {
+      startTime = aStartTime;
+      endTime = anEndTime;
+      totalShortfallDemand = demand;
+      totalFilled = filled;
+
+  }
+
+
+  public long getStartTime() { return startTime; }
+  public long getEndTime() { return endTime; }
+  public double getTotalDemand() { return totalShortfallDemand; }
+  public double getTotalFilled() { return totalFilled; }
+
+  public int getNumBuckets(long msecPerBucket) { return ((int) (((endTime + 1) - startTime)/msecPerBucket)); }
+  public double getShortfallQty() { return (totalShortfallDemand-totalFilled); }
+  public double getPercentShortfall() { return ((getShortfallQty()/getTotalDemand()) * 100); }
+
+  public void setTotalDemand(double demand) 
+    { totalShortfallDemand = demand; }
+
+  public void setTotalFilled(double filled) 
+    { totalFilled = filled; }
+
+  public String toString() {
+    StringBuffer sb = new StringBuffer("");
+    sb.append("\nStartTime=" + TimeUtils.dateString(getStartTime()));
+    sb.append(" - EndTime=" + TimeUtils.dateString(getEndTime()));
+    sb.append("\nShortfallQty=" + getShortfallQty());
+    sb.append(",PercentShortfall=" + getPercentShortfall());
+    return sb.toString();
+  }
+
+  public boolean equals(ShortfallPeriod sp) {
+      return ((getStartTime() == sp.getStartTime()) &&
+              (getEndTime() == sp.getEndTime()) &&
+	      (getTotalDemand() == sp.getTotalDemand()) &&
+	      (getTotalFilled() == sp.getTotalFilled()));
+  }
+
+
+  public boolean isPersistable() {
+    return true;
+  }
+}
