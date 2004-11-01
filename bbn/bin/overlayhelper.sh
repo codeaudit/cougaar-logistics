9efe0ff8bc45f070447da72977da0dc9c098b546
tom <tom>
2004-11-01 16:50:34 +0000

    Added ui to directories in overlay

--- bbn/bin/overlayhelper.sh
+++ bbn/bin/overlayhelper.sh
@@ -5,6 +5,7 @@ zipbase=$2
 module=$3
 cd ../
 cp -r data/csmart ${zipbase}/${module}/
+cp -r data/ui ${zipbase}/${module}/
 cp -r bin/ ${zipbase}/${module}/
 cd ${zipbase}/${module}
 zip -r ../../${zipname} .
