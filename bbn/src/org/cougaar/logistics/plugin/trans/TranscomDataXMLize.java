eadcfd4427dcb8d361b19765963f50e0d5068b47
gvidaver <gvidaver>
2007-02-12 17:59:48 +0000

    merged in changes from branch to use NamedPosition

--- bbn/src/org/cougaar/logistics/plugin/trans/TranscomDataXMLize.java
+++ bbn/src/org/cougaar/logistics/plugin/trans/TranscomDataXMLize.java
@@ -25,43 +25,32 @@
  */
 package org.cougaar.logistics.plugin.trans;
 
+import org.cougaar.glm.ldm.asset.Container;
+import org.cougaar.glm.ldm.asset.GLMAsset;
+import org.cougaar.glm.ldm.asset.Organization;
+import org.cougaar.glm.util.GLMMeasure;
+import org.cougaar.glm.util.GLMPreference;
+import org.cougaar.glm.util.GLMPrepPhrase;
+import org.cougaar.lib.util.UTILAsset;
+import org.cougaar.lib.vishnu.client.custom.CustomDataXMLize;
+import org.cougaar.logistics.ldm.Constants;
+import org.cougaar.planning.ldm.asset.AggregateAsset;
+import org.cougaar.planning.ldm.asset.Asset;
+import org.cougaar.planning.ldm.asset.AssetGroup;
+import org.cougaar.planning.ldm.measure.Distance;
+import org.cougaar.planning.ldm.measure.Latitude;
+import org.cougaar.planning.ldm.measure.Longitude;
+import org.cougaar.planning.ldm.plan.LatLonPoint;
+import org.cougaar.planning.ldm.plan.NamedPosition;
+import org.cougaar.planning.ldm.plan.Relationship;
+import org.cougaar.planning.ldm.plan.Role;
+import org.cougaar.planning.ldm.plan.Task;
+import org.cougaar.util.TimeSpan;
+import org.cougaar.util.log.Logger;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
-
-import org.cougaar.glm.ldm.asset.Container;
-import org.cougaar.glm.ldm.asset.GLMAsset;
-import org.cougaar.glm.ldm.asset.Organization;
-import org.cougaar.glm.ldm.plan.GeolocLocation;
-
-import org.cougaar.glm.util.GLMMeasure;
-import org.cougaar.glm.util.GLMPreference;
-import org.cougaar.glm.util.GLMPrepPhrase;
-
-import org.cougaar.lib.util.UTILAsset;
-
-import org.cougaar.lib.vishnu.client.custom.CustomDataXMLize;
-
-import org.cougaar.logistics.ldm.Constants;
-import org.cougaar.logistics.plugin.trans.GLMTransConst;
-
-import org.cougaar.planning.ldm.asset.AggregateAsset;
-import org.cougaar.planning.ldm.asset.Asset;
-import org.cougaar.planning.ldm.asset.AssetGroup;
-
-import org.cougaar.planning.ldm.measure.Distance;
-
-import org.cougaar.planning.ldm.plan.Relationship;
-import org.cougaar.planning.ldm.plan.RelationshipSchedule;
-import org.cougaar.planning.ldm.plan.RelationshipType;
-import org.cougaar.planning.ldm.plan.Role;
-import org.cougaar.planning.ldm.plan.Task;
-
-import org.cougaar.util.TimeSpan;
-import org.cougaar.util.log.Logger;
-
-import org.w3c.dom.Element;
 
 /**
  * Create either an XML document in the Vishnu Data format or Vishnu objects from ALP objects. <p>
@@ -77,6 +66,9 @@ public class TranscomDataXMLize extends CustomDataXMLize {
   protected UTILAsset assetHelper;
   protected Organization self;
 
+  private static double EARTH_RADIUS = 3437.75d; // nmi
+  private static double DEGREES_TO_RADIANS = (3.1415927d/180.0d);
+
   public TranscomDataXMLize (boolean direct, Logger logger, Set includeSet) {
     super (direct, logger);
     glmPrepHelper = new GLMPrepPhrase (logger);
@@ -86,16 +78,16 @@ public class TranscomDataXMLize extends CustomDataXMLize {
     this.assetIncludeSet = includeSet;
     this.assetHelper = new UTILAsset (logger);
   }
-  
+
   public boolean interestingTask(Task t) {
     boolean hasTransportVerb = t.getVerb().equals (Constants.Verb.TRANSPORT);
 
     return hasTransportVerb;
   }
-  
-  /** 
+
+  /**
    * Create XML for asset, subclass to add fields <p>
-   * 
+   *
    * Ignore everything but GlobalAir, GlobalSea, and the null asset.
    *
    * Uses statics for asset names from TranscomVishnuPlugin.
@@ -116,26 +108,26 @@ public class TranscomDataXMLize extends CustomDataXMLize {
       type = getOrganizationRole (asset);
       String name = getAssetName(asset);
       String parentType = "Asset";
-	  
+
       dataHelper.createField(object, parentType, "id", asset.getUID().toString());
       dataHelper.createField(object, parentType, "type", type);
       dataHelper.createField(object, parentType, "name", name);
 
       if (logger.isDebugEnabled())
-	logger.debug ("TranscomDataXMLize.processAsset - asset " + asset.getUID() + 
-		     " type " + type + " name " + name);
+        logger.debug ("TranscomDataXMLize.processAsset - asset " + asset.getUID() +
+          " type " + type + " name " + name);
     }
     else
       type = setName ("Asset", object, asset);
-	
+
     if (direct && logger.isDebugEnabled())
       logger.debug ("TranscomDataXMLize.processAsset - created resource " + object);
-	
+
     // ignore all but global air, sea, and nomenclature
     return (assetIncludeSet.contains (type));
   }
 
-  /** 
+  /**
    * Get the relationship role of the org, where we're only really interested 
    * in GlobalSea (SeaTransportationProvider) and GlobalAir (AirTransportationProvider)
    */
@@ -144,16 +136,16 @@ public class TranscomDataXMLize extends CustomDataXMLize {
     if (org.isSelf())
       self = org;
 
-    Collection providers = 
+    Collection providers =
       org.getRelationshipSchedule().getMatchingRelationships(Constants.RelationshipType.PROVIDER_SUFFIX,
-							     TimeSpan.MIN_VALUE,
-							     TimeSpan.MAX_VALUE);
+        TimeSpan.MIN_VALUE,
+        TimeSpan.MAX_VALUE);
 
     if (providers == null || providers.isEmpty()) {
       if (logger.isInfoEnabled()) {
-	logger.info ("TranscomDataXMLize.getOrganizationRole - no providers for " + org + " schedule is " +
-		     org.getRelationshipSchedule() +
-		     " so getting superiors (which should be a TRANSCOM agent).");
+        logger.info ("TranscomDataXMLize.getOrganizationRole - no providers for " + org + " schedule is " +
+          org.getRelationshipSchedule() +
+          " so getting superiors (which should be a TRANSCOM agent).");
       }
 
       /*
@@ -203,29 +195,29 @@ public class TranscomDataXMLize extends CustomDataXMLize {
 
     if (providers.size () > 1) {
       if (logger.isInfoEnabled()) {
-	logger.info ("TranscomDataXMLize.getOrganizationRole - NOTE - org " + org + 
-		     " has multiple providers :");
-	if (logger.isInfoEnabled()) {
-	  for (Iterator iter = providers.iterator(); iter.hasNext(); ) {
-	    logger.info ("\tprovider " + iter.next());
-	  }
-	}
+        logger.info ("TranscomDataXMLize.getOrganizationRole - NOTE - org " + org +
+          " has multiple providers :");
+        if (logger.isInfoEnabled()) {
+          for (Iterator iter = providers.iterator(); iter.hasNext(); ) {
+            logger.info ("\tprovider " + iter.next());
+          }
+        }
       }
       return "MANY_PROVIDERS";
     }
-	  
+
     if (providers.isEmpty ()) {
       if (logger.isInfoEnabled()) {
-	logger.info ("TranscomDataXMLize.getOrganizationRole - Note - no providers for " + org + 
-		     " schedule is " + org.getRelationshipSchedule());
+        logger.info ("TranscomDataXMLize.getOrganizationRole - Note - no providers for " + org +
+          " schedule is " + org.getRelationshipSchedule());
       }
 
       return "NO_PROVIDERS";
     }
 
     if (providers.size() == 1)
-      if (logger.isInfoEnabled()) 
-	logger.info ("found provider for " + org + " schedule was " + org.getRelationshipSchedule());
+      if (logger.isInfoEnabled())
+        logger.info ("found provider for " + org + " schedule was " + org.getRelationshipSchedule());
 
     Relationship relationToSuperior = (Relationship) providers.iterator().next();
     Role subRoleA = relationToSuperior.getRoleA();
@@ -235,12 +227,12 @@ public class TranscomDataXMLize extends CustomDataXMLize {
     // don't want the converse one - it seems random which org gets to be A and which B
     return (subRoleA.getName().startsWith ("Converse") ? subRoleB.getName () : subRoleA.getName());
   }
-  
-  /** 
+
+  /**
    * Create XML for task, subclass to add fields <p>
    *
    * Adds departure, arrival, from, to, name, type, and isPerson fields.
-   * 
+   *
    * NOTE : field names should match those in .dff file
    *
    * @param object node representing task
@@ -250,8 +242,8 @@ public class TranscomDataXMLize extends CustomDataXMLize {
   protected boolean processTask (Object object, Object taskOrAsset) {
     super.processTask (object, taskOrAsset);
     Task task = (Task) taskOrAsset;
-    String taskName = getTaskName (); 
-	
+    String taskName = getTaskName ();
+
     //	if (logger.isDebugEnabled())
     //	  logger.debug ("TranscomDataXMLize.processTask - uid " + task.getUID());
 
@@ -259,66 +251,66 @@ public class TranscomDataXMLize extends CustomDataXMLize {
 
     Date arrival = new Date(glmPrefHelper.getBestDate(task).getTime());
     dataHelper.createDateField(object, "arrival", arrival);
-    GeolocLocation from = glmPrepHelper.getFromLocation (task);
-    GeolocLocation to   = glmPrepHelper.getToLocation (task);
+    NamedPosition from = getFromLocation (task);
+    NamedPosition to   = getToLocation (task);
 
     try {
       if (direct) {
-	dataHelper.createGeoloc (object, "from", from);
-	dataHelper.createGeoloc (object, "to",   to);
+        dataHelper.createGeoloc (object, "from", from);
+        dataHelper.createGeoloc (object, "to",   to);
       }
       else {
-	Object field = dataHelper.createField (object, taskName, "from");
-	dataHelper.createGeoloc (field, "from", from);
-	field = dataHelper.createField (object, taskName, "to");
-	dataHelper.createGeoloc (field, "to",   to);
+        Object field = dataHelper.createField (object, taskName, "from");
+        dataHelper.createGeoloc (field, "from", from);
+        field = dataHelper.createField (object, taskName, "to");
+        dataHelper.createGeoloc (field, "to",   to);
       }
     } catch (Exception e) {
-      logger.error ("TranscomDataXMLize.createDoc - ERROR - " + 
-		    " no from or to geoloc on " + task);
+      logger.error ("TranscomDataXMLize.createDoc - ERROR - " +
+        " no from or to geoloc on " + task);
     }
 
     try {
       dataHelper.createField(object, taskName, "name", getAssetName(task.getDirectObject()));
       dataHelper.createField(object, taskName, "type", getAssetType(task.getDirectObject()));
     } catch (Exception e) {
-      logger.error ("TranscomDataXMLize.createDoc - ERROR - " + 
-	     " no type id pg on direct object of " + task);
+      logger.error ("TranscomDataXMLize.createDoc - ERROR - " +
+        " no type id pg on direct object of " + task);
     }
 
     GLMAsset baseAsset = null;
     Asset directObject = task.getDirectObject ();
-	
+
     if (directObject instanceof AggregateAsset) {
       baseAsset = (GLMAsset) ((AggregateAsset)directObject).getAsset ();
-    } 
+    }
     else {
       try {
-	if (task.getDirectObject () instanceof AssetGroup) {
-	  if (logger.isInfoEnabled())
-	    logger.info("processTasks - got asset group for task " + task.getUID());
-	  baseAsset = getBaseAsset ((AssetGroup) task.getDirectObject ());
-	}
-	else {
-	  baseAsset = (GLMAsset)directObject;
-	}
+        if (task.getDirectObject () instanceof AssetGroup) {
+          if (logger.isInfoEnabled())
+            logger.info("processTasks - got asset group for task " + task.getUID());
+          baseAsset = getBaseAsset ((AssetGroup) task.getDirectObject ());
+        }
+        else {
+          baseAsset = (GLMAsset)directObject;
+        }
       } catch (ClassCastException cce) {
-	logger.error ("TranscomDataXMLize.processTask - ERROR for task " + task +
-		      "\nDirectObject was not a GLMAsset, as expected.");
+        logger.error ("TranscomDataXMLize.processTask - ERROR for task " + task +
+          "\nDirectObject was not a GLMAsset, as expected.");
       }
     }
 
     addTaskPersonField (object, baseAsset);
     if (object == null)
-      logger.error ("huh? object is null for task " + task.getUID() + " verb " + task.getVerb() + 
-		    " is there a field missing for the task in the format file?");
+      logger.error ("huh? object is null for task " + task.getUID() + " verb " + task.getVerb() +
+        " is there a field missing for the task in the format file?");
     addIsAmmoField     (object, baseAsset);
 
-    float distance = (float) 
+    float distance = (float)
       ((glmPrepHelper.hasPrepNamed (task, GLMTransConst.SEAROUTE_DISTANCE)) ?
-       ((Distance) glmPrepHelper.getIndirectObject (task, 
-						    GLMTransConst.SEAROUTE_DISTANCE)).getNauticalMiles() :
-       measureHelper.distanceBetween (from, to).getNauticalMiles());
+        ((Distance) glmPrepHelper.getIndirectObject (task,
+          GLMTransConst.SEAROUTE_DISTANCE)).getNauticalMiles() :
+        distanceBetween (from, to).getNauticalMiles());
 
     dataHelper.createFloatField(object, "distance", distance);
 
@@ -331,7 +323,55 @@ public class TranscomDataXMLize extends CustomDataXMLize {
     return true;
   }
 
-  /** 
+  private NamedPosition getFromLocation (Task t) {
+    return (NamedPosition) glmPrepHelper.getIndirectObject (t, org.cougaar.glm.ldm.Constants.Preposition.FROM);
+  }
+
+  private NamedPosition getToLocation (Task t) {
+    return (NamedPosition) glmPrepHelper.getIndirectObject (t, org.cougaar.glm.ldm.Constants.Preposition.TO);
+  }
+
+  /**
+   * Utility function to calculate the distance between two locations
+   * @param start GeolocLocation starting point
+   * @param end GeolocLocation ending point
+   * @return Distance between the two points
+   */
+  public Distance distanceBetween(LatLonPoint start, LatLonPoint end) {
+    return distanceBetween(start, end, 1.0);
+  }
+
+  /**
+   * Utility function to calculate the distance between two locations
+   * @param start GeolocLocation starting point
+   * @param end GeolocLocation ending point
+   * @param multiplier Multiplier for the final dist result
+   * @return Distance between the two points
+   */
+  public Distance distanceBetween(LatLonPoint start, LatLonPoint end, double multiplier) {
+    // get Long/Lat
+    Longitude startlong = start.getLongitude();
+    Latitude startlat  = start.getLatitude();
+    Longitude endlong   = end.getLongitude();
+    Latitude  endlat    = end.getLatitude();
+
+    // get radian measures
+    double startlongrad = (startlong.getDegrees()*DEGREES_TO_RADIANS);
+    double startlatrad = (startlat.getDegrees()*DEGREES_TO_RADIANS);
+    double endlongrad = (endlong.getDegrees()*DEGREES_TO_RADIANS);
+    double endlatrad  = (endlat.getDegrees()*DEGREES_TO_RADIANS);
+
+    // calculate distance
+    double deltalong = startlongrad - endlongrad;
+    double distinradians = Math.acos(Math.sin(startlatrad)*Math.sin(endlatrad) +
+      Math.cos(startlatrad)*Math.cos(endlatrad)*
+        Math.cos(deltalong));
+    double retval = EARTH_RADIUS * distinradians;
+
+    return Distance.newNauticalMiles(retval * multiplier);
+  }
+
+  /**
    * try to determine base asset of asset group 
    * Basically, we're trying to determine whether this is ammo or not, or people or not
    * and we need to go down the nested sets of things until we find atomic assets.
@@ -342,16 +382,16 @@ public class TranscomDataXMLize extends CustomDataXMLize {
     for (Iterator iter = assets.iterator (); iter.hasNext () && base == null;) {
       Asset subasset = (Asset) iter.next();
       if (subasset instanceof AssetGroup) {
-	base = getBaseAsset ((AssetGroup) subasset); // recurse
+        base = getBaseAsset ((AssetGroup) subasset); // recurse
       }
       else if (subasset instanceof AggregateAsset) {
-	base = (GLMAsset) ((AggregateAsset) subasset).getAsset();
+        base = (GLMAsset) ((AggregateAsset) subasset).getAsset();
       }
       else if (subasset instanceof GLMAsset) {
-	base = (GLMAsset) subasset;
+        base = (GLMAsset) subasset;
       }
       else
-	logger.warn ("huh? unknown asset class " + subasset.getClass () + " for asset " + subasset);
+        logger.warn ("huh? unknown asset class " + subasset.getClass () + " for asset " + subasset);
     }
 
     if (!(base instanceof GLMAsset))
@@ -364,7 +404,7 @@ public class TranscomDataXMLize extends CustomDataXMLize {
   protected void addTaskPersonField (Object object, GLMAsset baseAsset) {
     dataHelper.createBooleanField(object, "isPerson", isPerson (baseAsset));
   }
-  
+
   protected boolean isPerson (GLMAsset asset) {	return asset.hasPersonPG ();  }
 
   protected void addIsAmmoField (Object object, GLMAsset baseAsset) {
@@ -372,14 +412,14 @@ public class TranscomDataXMLize extends CustomDataXMLize {
       logger.error ("huh? object is null testing " + baseAsset);
 
     boolean isContainer = isContainer (baseAsset);
-    dataHelper.createField(object, "Transport", "isAmmo", 
-			   (isContainer ? (isAmmo (baseAsset) ? TRUE : FALSE) : FALSE));
+    dataHelper.createField(object, "Transport", "isAmmo",
+      (isContainer ? (isAmmo (baseAsset) ? TRUE : FALSE) : FALSE));
   }
 
   protected boolean isContainer (GLMAsset asset) {
     LowFidelityAssetPG currentLowFiAssetPG = (LowFidelityAssetPG)
       asset.resolvePG (LowFidelityAssetPG.class);
-    
+
     if (currentLowFiAssetPG != null) {
       return currentLowFiAssetPG.getCCCDim().getIsContainer();
     }
@@ -388,7 +428,7 @@ public class TranscomDataXMLize extends CustomDataXMLize {
     }
   }
 
-  /** 
+  /**
    * An asset is an ammo container if it has a contents pg, since
    * only the Ammo Packer put a contents pg on a container.
    *
